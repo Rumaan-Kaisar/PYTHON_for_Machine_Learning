278cfe2e228ed17a5c15b8707751fe99d58bf3e0
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-10-11 13:37:09 +0600

    Add files via upload

--- create_folder.py
+++ create_folder.py
@@ -1,17 +1,10 @@
 import os
 
 # Directory: List of the folders you want to create
-directories = ['py_ch_1_intro',
-'py_ch_2_countables',
-'py_ch_3_control_statement',
-'py_ch_4_functions',
-'py_ch_5_OOP',
-'py_ch_6_modules_packages',
-'py_ch_7_exception',
-'py_ch_8_file_IO',
-'py_ch_9_Decorators_Generators',
-'py_ch_10_GUIs',
-'py_ch_11_adv_obj_DS']
+directories = ["py4DS_ch_1_basics",
+"py4DS_ch_2_dataFrame",
+"py4DS_ch_3_matrices",
+"py4DS_ch_4_VIZ_p1"]
 
 # Parent Directory path
 # parent_dir = "D:/Pycharm projects/GeeksForGeeks/Authors"
@@ -32,3 +25,7 @@ for i in range (0, len(directories)):
 # run windows cmd/terminal inside the working directory
 # python create_folder.py
 
+# "py4DS_ch_1_basics",
+# "py4DS_ch_2_dataFrame",
+# "py4DS_ch_3_matrices",
+# "py4DS_ch_4_VIZ_p1"
