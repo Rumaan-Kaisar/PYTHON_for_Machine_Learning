db2755a968523334e7064baea8ab1ec420844942
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-10-14 14:18:02 +0600

    Update pyDS_ch1_1_variables_operators.py

--- py4DS_ch_1_basics/pyDS_ch1_1_variables_operators.py
+++ py4DS_ch_1_basics/pyDS_ch1_1_variables_operators.py
@@ -1,1 +1,15 @@
-# Courses: A-Z PY for Data-Science    0
+
+# Courses: A-Z PY for Data-Science    1, 
+
+# ----------------    Anaconda, jupyter Installation    ----------------
+    # install anaconda
+    # open command line/terminal
+    # run jupyter notebook
+
+
+# ----------------    Run jupyter in VS-code    ----------------
+# But we're gonna use VS-code to run "Jupyter Notebook - ipynb" files
+    # install ipykernel package (vs-code): Running cells with 'Python 3.10.3 64-bit' requires ipykernel package.
+    # Allow VS-code in windows sequrity (if pop-up appears)
+
+
