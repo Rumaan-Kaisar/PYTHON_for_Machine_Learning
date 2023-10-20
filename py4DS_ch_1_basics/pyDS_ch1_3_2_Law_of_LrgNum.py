03180d03df9598c7d02f3559536a7a31d9dafce8
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-10-20 16:49:19 +0600

    Add files via upload

---
+++ py4DS_ch_1_basics/pyDS_ch1_3_2_Law_of_LrgNum.py
@@ -0,0 +1,33 @@
+
+################# 1.10: 6:40
+# copy:  
+#        
+#        
+################# (18-oct-23 for 20-oct-23)
+
+# Courses: A-Z PY for Data-Science    1.10, 7.1
+
+# -------------    Law of Large numbers (LLN)    -------------
+# Simple but Powerful concept:
+# Consider the "coin toss example"
+    # we know we have 50/50 chance of getting HEAD or TAIL.
+        # But if we toss the coin for a very fiew time say 10 times, it wont be 50-50
+            # toss 10 times: HEAD : TAIL    =  70% : 30%
+            # toss 100 times: HEAD : TAIL    =  60% : 40%
+            # toss 1000 times: HEAD : TAIL    =  55% : 45%
+            # toss 10000 times: HEAD : TAIL    =  51% : 49%
+
+
+# Law of Large numbers (LLN):
+    # Say we have a sample, the 'average of the sample' converges to EXPECTED VALUE of that sample 
+        # if the 'number of observations' tends to infinity.
+
+
+
+# -----------    Normal distribution of random numbers    -----------
+# We got a bell-shaped Normal-Distribution curve
+# It says: 
+    # There is 34.1% probability that the random number lies between (0, 1)
+    # There is 34.1% probability that the random number lies between (-1, 0)
+    # There is total 68.2% probability that the random number lies between (-1, 1)
+
