428fd413fa1c9cc648650da64f2b93b64c2f5501
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-11-07 02:07:42 +0600

    Update pyDS_ch3_1_prj_info_Bsktbl.py

--- py4DS_ch_3_matrices/pyDS_ch3_1_prj_info_Bsktbl.py
+++ py4DS_ch_3_matrices/pyDS_ch3_1_prj_info_Bsktbl.py
@@ -1,8 +1,52 @@
 
-################# 348: FULL
+################# 4.1: FULL
 # copy:  
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (5-nov-23 for 7-nov-23)
 
 # Courses: A-Z PY for Data-Science    0
+
+# Objective: 
+    # we'll try to learn MATRICES
+    # we'll use real-world example: NBA-Basketball data
+
+
+# HIGHEST PAID PLAYERS 2015-2016 (wikipedia)
+
+#   Player 	        Salary	        Team
+
+#   Kobe Bryant	    $25,000,000	    Los Angeles Lakers
+#   Joe Johnson	    $24,894,863	    Brooklyn Nets
+#   LeBron James	$22,970,500	    Cleveland Cavaliers
+#   Carmelo Anthony	$22,875,000	    New York Knicks
+#   Dwight Howard	$22,359,364	    Houston Rockets
+#   Chris Bosh	    $22,192,730	    Miami Heat
+#   Chris Paul	    $21,468,695	    Los Angeles Clippers
+#   Kevin Durant	$20,158,622	    Oklahoma City Thunder
+#   Derrick Rose	$20,093,064	    Chicago Bulls
+#   Dwyane Wade	    $20,000,000	    Miami Heat
+
+
+
+# We'll analyze the data for those 10 players
+    # How they are playing for the past 10 years
+    # analyze their performance-patterns in the past ten years
+    # How they become top-10 players, how about their previous performance
+
+
+# about Data-Set:
+    # we'll  anlyze following parameters against years 2005 to 2015
+    # GP: Total games played by the player
+    # MPG: Minutes per game, total minutes played in a season
+    # FG%: field goal percentage. (Passed the the ball through the hoop)
+        # We'll get this from actual 'Field Goal' and 'Field Goal Attempts'
+    # PPG: Points per game. Total points scored in a season
+
+# We'll create matrices:
+    # - Salary
+    # - Games
+    # - MinutesPlayed
+    # - FieldGoals
+    # - FieldGoalAttempts
+    # - Points
