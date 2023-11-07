83adf9fc4b8e7c5debb5b4d09bd73e5e4264ed04
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-11-08 03:40:14 +0600

    Update pyDS_ch3_2_mtrix_dict.py

--- py4DS_ch_3_matrices/pyDS_ch3_2_mtrix_dict.py
+++ py4DS_ch_3_matrices/pyDS_ch3_2_mtrix_dict.py
@@ -1,8 +1,45 @@
 
-################# 348: FULL
+################# 4.2: FULL
 # copy:  
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (7-nov-23 for 8-nov-23)
 
-# Courses: A-Z PY for Data-Science    0
+# Courses: A-Z PY for Data-Science    4.2, 4.3, 4.4, 4.5
+
+
+# since we'll work with data-frames in Data-science, we need to understand MATRICES
+
+
+# --------------    Matrix INDEXATION in python    --------------
+
+# locate a single element in a 2D matrix
+# matrix[row, column], and both start from 0
+A[2, 3]     # an elemnt from 3rd-row and 4-th column
+
+
+# locate a ROW      matrix[row, : ]
+A[1, :]     # 2nd row
+
+
+# locate a COLUMN   matrix[ :, col]
+A[:, 3]     # 4th column
+
+
+
+
+# --------------------    Our First Matrix    --------------------
+# Import numpy
+# Create a List-of-Lists
+# convert the to 2D-array (matrix) using np.array
+
+import numpy as np
+
+a = [.00, .01, .02]
+b = [.10, .11, .12]
+c = [.20, .21, .22]
+
+mat_1 = np.array([a, b, c])
+
+
+# python pyDS_ch3_2_mtrix_dict.py
