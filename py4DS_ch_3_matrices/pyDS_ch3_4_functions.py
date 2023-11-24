eae763a89736ecd8cf2e478b06342a238475d525
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-11-24 19:53:28 +0600

    Update pyDS_ch3_4_functions.py

--- py4DS_ch_3_matrices/pyDS_ch3_4_functions.py
+++ py4DS_ch_3_matrices/pyDS_ch3_4_functions.py
@@ -1,10 +1,4 @@
 
-################# 4.8: full, 4.9: 1:00
-# copy:  
-#        
-#        
-################# (21-nov-23 for 22-nov-23)
-
 # Courses: A-Z PY for Data-Science    4.8, 4.9
 
 # --------    Functions intro    --------
@@ -54,8 +48,9 @@ myPlot(["KobeBryant", "ChrisBosh", "DwayneWade"])
 # Changing Colors & Markers
 def myPlot(list_of_player_name):
     col_r = {"KobeBryant":"Black","JoeJohnson":"Red","LeBronJames":"Green","CarmeloAnthony":"Blue","DwightHoward":"Magenta","ChrisBosh":"Black","ChrisPaul":"Red","KevinDurant":"Green","DerrickRose":"Blue","DwayneWade":"Magenta"}
+    mr_kr = {"KobeBryant":"s","JoeJohnson":"o","LeBronJames":"^","CarmeloAnthony":"D","DwightHoward":"s","ChrisBosh":"o","ChrisPaul":"^","KevinDurant":"D","DerrickRose":"s","DwayneWade":"o"}
     for name in list_of_player_name:
-        plt.plot(Games[Pdict[name]], ls='-.', c=col_r[name], marker = 'D', ms=7, label = name)
+        plt.plot(Games[Pdict[name]], ls='-.', c=col_r[name], marker = mr_kr[name], ms=7, label = name)
     
     plt.legend(loc='lower left', bbox_to_anchor=(.5,.5))
     plt.legend(loc='upper left', bbox_to_anchor=(1,1))
@@ -63,13 +58,86 @@ def myPlot(list_of_player_name):
     plt.show()
 
 
+myPlot(["KobeBryant","LeBronJames","DerrickRose", "JoeJohnson"])
 
 
 
 
+# --------    More Generalization    --------
+    # in above code we only vizualize "Games" data (matrix)
+    # we now generalize our function to vizualize other Data (Points, Salary etc.)
+    # to do that we now introduce 'data' parameter
+        # 'data' needs to be a matrix
+
+# ----    more data    ----
+# Salaries
+KobeBryant_Salary = [15946875,17718750,19490625,21262500,23034375,24806250,25244493,27849149,30453805,23500000]
+JoeJohnson_Salary = [12000000,12744189,13488377,14232567,14976754,16324500,18038573,19752645,21466718,23180790]
+LeBronJames_Salary = [4621800,5828090,13041250,14410581,15779912,14500000,16022500,17545000,19067500,20644400]
+CarmeloAnthony_Salary = [3713640,4694041,13041250,14410581,15779912,17149243,18518574,19450000,22407474,22458000]
+DwightHoward_Salary = [4493160,4806720,6061274,13758000,15202590,16647180,18091770,19536360,20513178,21436271]
+ChrisBosh_Salary = [3348000,4235220,12455000,14410581,15779912,14500000,16022500,17545000,19067500,20644400]
+ChrisPaul_Salary = [3144240,3380160,3615960,4574189,13520500,14940153,16359805,17779458,18668431,20068563]
+KevinDurant_Salary = [0,0,4171200,4484040,4796880,6053663,15506632,16669630,17832627,18995624]
+DerrickRose_Salary = [0,0,0,4822800,5184480,5546160,6993708,16402500,17632688,18862875]
+DwayneWade_Salary = [3031920,3841443,13041250,14410581,15779912,14200000,15691000,17182000,18673000,15000000]
+#Matrix
+Salary = np.array([KobeBryant_Salary, JoeJohnson_Salary, LeBronJames_Salary, CarmeloAnthony_Salary, DwightHoward_Salary, ChrisBosh_Salary, ChrisPaul_Salary, KevinDurant_Salary, DerrickRose_Salary, DwayneWade_Salary])
+
+# Points
+KobeBryant_PTS = [2832,2430,2323,2201,1970,2078,1616,2133,83,782]
+JoeJohnson_PTS = [1653,1426,1779,1688,1619,1312,1129,1170,1245,1154]
+LeBronJames_PTS = [2478,2132,2250,2304,2258,2111,1683,2036,2089,1743]
+CarmeloAnthony_PTS = [2122,1881,1978,1504,1943,1970,1245,1920,2112,966]
+DwightHoward_PTS = [1292,1443,1695,1624,1503,1784,1113,1296,1297,646]
+ChrisBosh_PTS = [1572,1561,1496,1746,1678,1438,1025,1232,1281,928]
+ChrisPaul_PTS = [1258,1104,1684,1781,841,1268,1189,1186,1185,1564]
+KevinDurant_PTS = [903,903,1624,1871,2472,2161,1850,2280,2593,686]
+DerrickRose_PTS = [597,597,597,1361,1619,2026,852,0,159,904]
+DwayneWade_PTS = [2040,1397,1254,2386,2045,1941,1082,1463,1028,1331]
+#Matrix
+Points = np.array([KobeBryant_PTS, JoeJohnson_PTS, LeBronJames_PTS, CarmeloAnthony_PTS, DwightHoward_PTS, ChrisBosh_PTS, ChrisPaul_PTS, KevinDurant_PTS, DerrickRose_PTS, DwayneWade_PTS])
+
+
+# Generalize the function, to take diffent data-matrix as an argumnet
+def myPlot(data, list_of_player_name):
+    col_r = {"KobeBryant":"Black","JoeJohnson":"Red","LeBronJames":"Green","CarmeloAnthony":"Blue","DwightHoward":"Magenta","ChrisBosh":"Black","ChrisPaul":"Red","KevinDurant":"Green","DerrickRose":"Blue","DwayneWade":"Magenta"}
+    mr_kr = {"KobeBryant":"s","JoeJohnson":"o","LeBronJames":"^","CarmeloAnthony":"D","DwightHoward":"s","ChrisBosh":"o","ChrisPaul":"^","KevinDurant":"D","DerrickRose":"s","DwayneWade":"o"}
+    for name in list_of_player_name:
+        plt.plot(data[Pdict[name]], ls='-.', c=col_r[name], marker = mr_kr[name], ms=7, label = name)
+    
+    plt.legend(loc='lower left', bbox_to_anchor=(.5,.5))
+    plt.legend(loc='upper left', bbox_to_anchor=(1,1))
+    plt.xticks(list(range(0, len(Seasons))), Seasons, rotation = 'vertical')
+    plt.show()
+
+
+# Notice, we now pass the Data-matrix as an argumnet
+myPlot(Games, ["KobeBryant","LeBronJames","DerrickRose", "JoeJohnson"])     # To vizualize 'Games'
+myPlot(Points, ["KobeBryant","LeBronJames","DerrickRose", "JoeJohnson"])     # To vizualize 'Points'
+myPlot(Salary, ["KobeBryant","LeBronJames","DerrickRose", "JoeJohnson"])     # To vizualize 'Salary'
 
 
 
 
+# --------    Default value of argumnet    --------
+# Default parameter: Specify a default value, in case the missing-argument 
+Players = ["KobeBryant","JoeJohnson","LeBronJames","CarmeloAnthony","DwightHoward","ChrisBosh","ChrisPaul","KevinDurant","DerrickRose","DwayneWade"]
+
+# Specify a 'default parameter/value' for list_of_player_name
+def myPlot(data, list_of_player_name = Players):
+    col_r = {"KobeBryant":"Black","JoeJohnson":"Red","LeBronJames":"Green","CarmeloAnthony":"Blue","DwightHoward":"Magenta","ChrisBosh":"Black","ChrisPaul":"Red","KevinDurant":"Green","DerrickRose":"Blue","DwayneWade":"Magenta"}
+    mr_kr = {"KobeBryant":"s","JoeJohnson":"o","LeBronJames":"^","CarmeloAnthony":"D","DwightHoward":"s","ChrisBosh":"o","ChrisPaul":"^","KevinDurant":"D","DerrickRose":"s","DwayneWade":"o"}
+    for name in list_of_player_name:
+        plt.plot(data[Pdict[name]], ls='-.', c=col_r[name], marker = mr_kr[name], ms=7, label = name)
+    
+    plt.legend(loc='lower left', bbox_to_anchor=(.5,.5))
+    plt.legend(loc='upper left', bbox_to_anchor=(1,1))
+    plt.xticks(list(range(0, len(Seasons))), Seasons, rotation = 'vertical')
+
+
+# Notice, we didn't specify any value in the 2nd plot
+myPlot(Games, ["KobeBryant","LeBronJames","DerrickRose", "JoeJohnson"]) 
+myPlot(Games)     # no specific players
 
 
