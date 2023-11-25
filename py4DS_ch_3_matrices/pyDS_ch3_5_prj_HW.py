5bd06bfc1c6f5b82e87cedb7944320872c759c4e
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-11-25 20:48:13 +0600

    Update pyDS_ch3_5_prj_HW.py

--- py4DS_ch_3_matrices/pyDS_ch3_5_prj_HW.py
+++ py4DS_ch_3_matrices/pyDS_ch3_5_prj_HW.py
@@ -1,8 +1,43 @@
 
-################# 348: FULL
+################# 4.10: 1:28
 # copy:  
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (24-nov-23 for 25-nov-23)
 
-# Courses: A-Z PY for Data-Science    0
+# Courses: A-Z PY for Data-Science    4.10, 4.11, 4.12
+
+
+# --------    Project : BASKETBALL INSIGHTS    --------
+# In this section we'll analyze the 'NBA'-data for Different "Basketball-players"
+# We'll use our previous function (it takes Data as paramters)
+
+# -----------    We'll focus on "Analytics" and "Data-Science"    -----------
+# Basically in this part we dont code anything new
+    # But we'll try to understand the data in the "Data-Analysis" prespective
+        # Well try to figure-out anomolies 
+        # Which data is correlated to another data
+
+
+
+
+# --------    Salary Metrices (salary vs in-game statistics)    --------
+# Salary
+# Salary-per game
+# Salary-per FieldGoals
+
+# Coclusion "salary vs in-game statistics" might not be a good option
+    # Salary is not effected by injuries
+    # but "in-game statistics" is effected by injuries
+
+
+
+ 
+# --------    In-game Metrices (in-game statistics only)    --------
+# (walk through 3:58)
+# Minuets Played
+# points
+
+
+
+
