636f684a60afc3a450e4e66da6d1722ffe652dda
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-12-03 21:01:16 +0600

    Update pyDS_ch3_5_prj_HW.py

--- py4DS_ch_3_matrices/pyDS_ch3_5_prj_HW.py
+++ py4DS_ch_3_matrices/pyDS_ch3_5_prj_HW.py
@@ -363,3 +363,71 @@ myPlot(Points/FieldGoals)
 
 
 
+# --------    Hints    --------
+# The matrix calculation for part 3 is:
+    # (Points - FreeThrows)/FieldGoals
+
+# Steps:
+# ----  Prepare Data  ----
+# library import
+# Seasons & Season-dictionary
+# Players & Players-dictionary
+# Free Throws (Matrix Data)
+# Free Throws Atempts (Matrix Data)
+# Check matices FreeThrows, FreeThrowAttempts
+# Define function
+
+
+# ----  Visualize 1  ----
+# Visualize 'FreeThrows'
+# Visualize 'FreeThrowAttempts'
+
+
+# ----  Visualize 2 : Free Throw Attempts per Games  ----
+# Games (Matrix Data)
+# Visualize 'FreeThrowAttempts / Games'
+    # Notice how Chris Paul gets few attempts per game
+
+
+# ----  Visualize 3 : Free Throw Accuracy  ----
+# Visualize 'Free Throw Accuracy = FreeThrows / FreeThrowAttempts'
+    # And yet Chris Paul's accuracy is one of the highest Chances are his team would get more points if he had more FTA's.
+    # Also notice that Dwight Howard's FT Accuracy is extremely poor compared to other players. If you recall, Dwight Howard's Field Goal Accuracy was exceptional:
+
+
+# ----  Visualize 4 : Field Goal Accuracy  ----
+# FieldGoals (Matrix Data)
+# FieldGoalattempts (Matrix Data)
+# Visualize 'Field Goal Accuracy = FieldGoals / FieldGoalattempts'
+    # How could this be? Why is there such a drastic difference?
+    # We will see just now...
+
+
+# ----  Visualize 5 : Points excluding FreeThrows  ----
+# Points (Matrix Data)
+# Visualize Player Style Patterns Excluding Free Throws '(Points - FreeThrows)/FieldGoals'
+
+""" 
+Because we have excluded free throws, this plot now shows us 
+the true representation of player style change. We can verify
+that this is the case because all the marks without exception
+on this plot are between 2 and 3. That is because Field Goals
+can only be for either 2 points or 3 points.
+
+Insights:
+1. You can see how players' preference for 2 or 3 point shots
+   changes throughout their career. We can see that almost all
+   players in this dataset experiment with their style throughout
+   their careers. Perhaps, the most drastic change in style has
+   been experienced by Joe Johnson.
+
+2. There is one exception. You can see that one player has not
+   changed his style at all - almost always scoring only 2-pointers.
+   Who is this mystert player? It's Dwight Howard! 
+   Now that explains a lot. The reason that Dwight Howard's
+   Field Goal accuracy is so good is because he almost always
+   scores 2-pointers only. That means he can be close to the basket
+   or even in contact with it. Free throws, on the other hand require
+   the player to stand 15ft (4.57m) away from the hoop. That's 
+   probably why Dwight Howard's Free Throw Accuracy is poor.
+ """
