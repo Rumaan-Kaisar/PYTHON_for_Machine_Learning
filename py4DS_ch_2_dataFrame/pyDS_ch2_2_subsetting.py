d5a1b22837d9d6385d16f766a16d899ce541eebb
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-12-16 02:12:18 +0600

    Update pyDS_ch2_2_subsetting.py

--- py4DS_ch_2_dataFrame/pyDS_ch2_2_subsetting.py
+++ py4DS_ch_2_dataFrame/pyDS_ch2_2_subsetting.py
@@ -1,9 +1,9 @@
 
-################# 5.4: 6:35
+################# 5.4: 10:23
 # copy:  
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (15-dec-23 for 16-dec-23)
 
 # Courses: A-Z PY for Data-Science    5.4
 
@@ -61,3 +61,43 @@ stats
 
 # Get only every 20th country
 stats[::20]
+
+
+
+
+# ----  subset by columns  ----
+# To get the columns: Just specify the 'name of the column'
+    # and to get the ROWs just use 'slicing-technique'
+
+stats.columns    # Get all the name of the columns first
+# ['Country Name', 'Country Code', 'Birth rate', 'Internet users', 'Income Group']
+
+stats['Country Name']   # access the specific column
+# Note that aboove is not a data-frame
+    # if we use only one column, it wont be a data-frame
+    # it's some kind of list to do other staff
+
+# However, if we use 2 or more columns, we'll get a Data-frame
+    # we first need to create a "list of column-names": e.g. ['Country Name', 'Birth rate']
+    # at first-lok following may look like a 2d-matrix but the fist '[]' is with 'stats' object
+        # we're just passing a list into stats[] object
+        # In R you'd be passing a Vector: c('CountryName', 'BirthRate'), so in that case no '[[' happens
+stats[['Country Name', 'Birth rate']]
+
+# also note that, we can create our own ORDER
+stats[['Country Code', 'Country Name', 'Internet users']]
+
+
+# we can also use head() on those data-frames
+stats['Country Name'].head()
+stats[['Country Code', 'Country Name', 'Internet users']].head(15)
+
+
+
+
+# Convention!!
+
+
+
+
+
