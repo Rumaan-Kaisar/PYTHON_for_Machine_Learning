0490f86f39a0709cb48c9d6d774c83f323b6ae6a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-12-20 19:25:18 +0600

    Update pyDS_ch2_3_basic_opr.py

--- py4DS_ch_2_dataFrame/pyDS_ch2_3_basic_opr.py
+++ py4DS_ch_2_dataFrame/pyDS_ch2_3_basic_opr.py
@@ -1,10 +1,4 @@
 
-################# 5.5: 5:10
-# copy:  
-#        
-#        
-################# (17-dec-23 for 19-dec-23)
-
 # Courses: A-Z PY for Data-Science    5.5
 
 # --------    basic operations with a DATA FRAME    --------
@@ -24,7 +18,7 @@ stats[['Country Code','Birth rate','Internet users']][4:8]
 
 # --------    Mathematical opeartions    --------
 # very similar to Matix operations
-result = stats['Birth rate'] * stats['Internet users']  # multiply on ecolumn with another
+result = stats['Birth rate'] * stats['Internet users']  # multiply one column with another
 result.head()
 
 # rename to one words
@@ -56,5 +50,28 @@ stats['xyz'] = [1, 2, 3, 4, 5, 6]   # ERR: no recycling optioon
         # it can recycle the values if youu supply some values
         # basically it pouplates rest of the column by copying supplied values
 
+
+
+
+# --------    Remove a column from the dataframe    --------
+# you have to specify the axis, i.e. row = 1, column = 0
+    # axis : {0 or 'index', 1 or 'columns'}, default 0, i.e. removes row by default
+# parameter-name, use the name of the parameter if the order is known, eg. stats.drop('MyCalc', 1)
+    # eg: 'axis', 'index', 'labels' etc
+stats.drop('MyCalc', axis=1)    # removes column called 'MyCalc'
+stats.drop(index=[0, 1, 2])    # removes first 3 rows
+stats.drop(columns=['MyCalc_2'])    # removes column called 'MyCalc'
+
+# remove multiple columns
+stats.drop(columns=['CountryName', 'BirthRate'])    # removes columns 'CountryName', 'BirthRate'
+
+# 'stats' itself is unchanged
+stats.head()
+
+# ----  use assignmnet to uupdate  ----
+# Note that applying 'drop()' will 'return a new object'
+# so 'stats' is not changed, to update it we need 'assignment statement'
+stats = stats.drop(index=[0, 1, 2], columns=['CountryName', 'BirthRate'])
+stats.head()
 
 
