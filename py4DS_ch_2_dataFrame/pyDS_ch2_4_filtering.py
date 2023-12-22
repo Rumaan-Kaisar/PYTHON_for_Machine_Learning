49e112c835ebd6bce01a3922871d9b28475e990d
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-12-22 09:29:31 +0600

    Update pyDS_ch2_4_filtering.py

--- py4DS_ch_2_dataFrame/pyDS_ch2_4_filtering.py
+++ py4DS_ch_2_dataFrame/pyDS_ch2_4_filtering.py
@@ -1,8 +1,24 @@
 
-################# 348: FULL
+################# 5.6: 1:15
 # copy:  
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (20-dec-23 for 22-dec-23)
 
 # Courses: A-Z PY for Data-Science    5.6
+
+# Loading Dataset
+import pandas as pd
+stats = pd.read_csv('./DataDemographic.csv')    # load datset
+stats.head()    # view dataset
+
+
+# --------    Filtering Data-Frames    --------
+# filtering is important in Data-Science
+    # it helps to find 'insights' and subset the data in proper way
+
+# Filtering is all about ROWs
+    # we look at the columns to filter the rows
+    # eg: 'filter High Income', 'all countries starting with letter B'
+
+
