e833a94c54b7865c25943a2318eff4d6b50259ae
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-12-23 19:24:12 +0600

    Update pyDS_ch2_4_filtering.py

--- py4DS_ch_2_dataFrame/pyDS_ch2_4_filtering.py
+++ py4DS_ch_2_dataFrame/pyDS_ch2_4_filtering.py
@@ -1,15 +1,21 @@
 
-################# 5.6: 1:15
+################# 5.6: 8:00
 # copy:  
 #        
 #        
-################# (20-dec-23 for 22-dec-23)
+################# (22-dec-23 for 23-dec-23)
 
 # Courses: A-Z PY for Data-Science    5.6
 
 # Loading Dataset
+from msvcrt import open_osfhandle
+from os import O_RDONLY
 import pandas as pd
 stats = pd.read_csv('./DataDemographic.csv')    # load datset
+
+# rename to one words
+stats.columns = ['CountryName', 'CountryCode', 'BirthRate', 'InternetUsers', 'IncomeGroup']
+
 stats.head()    # view dataset
 
 
@@ -20,5 +26,29 @@ stats.head()    # view dataset
 # Filtering is all about ROWs
     # we look at the columns to filter the rows
     # eg: 'filter High Income', 'all countries starting with letter B'
+
+stats.InternetUsers < 2
+# It gives a list of 'True' and 'False'
+# These valuse tells us 'which ROWs meet the condition'
+
+filtered_1 = (stats.InternetUsers < 2)
+filtered_1.head(15)
+
+# Now we can filter the ROW's using these True-Flase values
+    # We gonna use our 'filtered_1' object
+
+# we'll use 'True-Flase values' to slicing ROWs
+stats[filtered_1]
+# conceptually this is juust like R
+
+
+
+
+# --------  More Filtering : Practice  --------
+filtered_2 = (stats.BirthRate > 40)
+stats[filtered_2]
+
+# Or we can put the 'condition' inside '[]'
+stats[stats.BirthRate > 40]
 
 
