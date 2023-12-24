72d6a62d2223a9381afa0028ded2d27fb076f40e
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-12-24 14:03:28 +0600

    Update pyDS_ch2_4_filtering.py

--- py4DS_ch_2_dataFrame/pyDS_ch2_4_filtering.py
+++ py4DS_ch_2_dataFrame/pyDS_ch2_4_filtering.py
@@ -1,15 +1,7 @@
 
-################# 5.6: 8:00
-# copy:  
-#        
-#        
-################# (22-dec-23 for 23-dec-23)
-
 # Courses: A-Z PY for Data-Science    5.6
 
 # Loading Dataset
-from msvcrt import open_osfhandle
-from os import O_RDONLY
 import pandas as pd
 stats = pd.read_csv('./DataDemographic.csv')    # load datset
 
@@ -50,5 +42,44 @@ stats[filtered_2]
 
 # Or we can put the 'condition' inside '[]'
 stats[stats.BirthRate > 40]
+
+
+
+
+# --------    combine two conditions    --------
+# We need to use 'bitwise and: &' instead of logical-and 'and'
+stats[(stats.BirthRate > 40) and (stats.InternetUsers < 2)] # gives ERR. Why??
+# ValueError: The truth value of a Series is ambiguous.
+    # '(stats.BirthRate > 40) and (stats.InternetUsers < 2)' causing the ERR
+    # because 'and' works with single value, it doesn't know how to work with 'list'
+    # Its tryin to convert the whole series/list (stats.BirthRate > 40) into a single 'True' or 'False' value
+    # Note that: in R we won't get this ERR, because R is 'vectorize-prgramming-language'
+
+# To resolve this, we need to use 'bitwise and: &'
+    # It'll deal with element-element level
+    # it's straight-forward if we think True=1 and False=0
+        # then ['False', 'False', 'False', 'True', 'True'] & ['False', 'False', 'False', 'True', 'False']
+        # is actually [0, 0, 0, 1, 1] & [0, 0, 0, 1, 0] = 00011 & 00010 = 00010
+filtered_1 & filtered_2
+stats[filtered_1 & filtered_2]
+
+# following also gives same result
+stats[(stats.BirthRate > 40) & (stats.InternetUsers < 2)] # gives ERR. Why?? Didn't use '()'
+
+
+
+# --------    More Filtering    --------
+# Filter Categorical data
+stats[stats.IncomeGroup == "High income"]
+
+# How to get all 'Unique Categories'
+stats.IncomeGroup.unique()  # shows all the 'categories' used in a column
+
+# Quick Exercise: Find specific data/row
+# Find out everything about the country 'Malta'
+stats[stats.CountryName == "Malta"]
+
+# Find out everything about the country 'Malta', 'Canada', 'China'. HINT: use 'Bitwise OR |'
+stats[(stats.CountryName == "Malta") | (stats.CountryName == "Canada") | (stats.CountryName == "China")]
 
 
