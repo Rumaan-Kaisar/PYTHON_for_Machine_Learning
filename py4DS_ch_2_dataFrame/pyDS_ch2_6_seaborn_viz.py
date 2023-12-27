6939e9d217ad0861460cb96e998f4038e2994c9a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-12-27 12:54:37 +0600

    Update pyDS_ch2_6_seaborn_viz.py

--- py4DS_ch_2_dataFrame/pyDS_ch2_6_seaborn_viz.py
+++ py4DS_ch_2_dataFrame/pyDS_ch2_6_seaborn_viz.py
@@ -1,8 +1,35 @@
 
-################# 348: FULL
+################# 5.8: 0.0
 # copy:  
 #        
 #        
 ################# (26-sep-23 for 27-sep-23)
 
 # Courses: A-Z PY for Data-Science    5.8, 5.9
+
+# ----------------      seaborn      ----------------
+# seaborn: statistical data visualization
+
+# seaborn is a very powerful package of python for vizualization
+    # Seaborn is a Python visualization library based on "matplotlib". 
+    # It provides a high-level interface for drawing attractive statistical graphics.
+
+    # Online documentation is available at 'seaborn.pydata.org'
+
+
+# install via CLI: pip install seaborn
+
+
+# 'matplotlib' is needed along with 'seaborn'
+
+
+# seaborn Example gallery : use the examples to plot something awesome 
+# https://seaborn.pydata.org/examples/index.html
+
+# kdeplot: cubehelix palettes : https://seaborn.pydata.org/examples/palette_generation.html
+# violinplot: Grouped violinplots with split violins : https://seaborn.pydata.org/examples/grouped_violinplots.html
+    # Violinplot from a wide-form dataset: https://seaborn.pydata.org/examples/wide_form_violinplot.html
+
+# Replacement for deprecated tsplot (not used anymore)
+    # Timeseries plot with error bands : https://seaborn.pydata.org/examples/errorband_lineplots.html
+  
