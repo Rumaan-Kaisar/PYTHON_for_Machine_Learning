e99481c1cbaaac7a3b378319e79bab517ad78ff2
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-12-29 16:15:11 +0600

    Update pyDS_ch2_6_seaborn_viz.py

--- py4DS_ch_2_dataFrame/pyDS_ch2_6_seaborn_viz.py
+++ py4DS_ch_2_dataFrame/pyDS_ch2_6_seaborn_viz.py
@@ -1,11 +1,22 @@
 
-################# 5.8: 0.0
+################# 5.8: 6:57
 # copy:  
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (27-dec-23 for 29-dec-23)
 
 # Courses: A-Z PY for Data-Science    5.8, 5.9
+
+# Loading Dataset
+import pandas as pd
+stats = pd.read_csv('./DataDemographic.csv')    # load datset
+
+# rename to one words
+stats.columns = ['CountryName', 'CountryCode', 'BirthRate', 'InternetUsers', 'IncomeGroup']
+
+stats.head()    # view dataset
+
+
 
 # ----------------      seaborn      ----------------
 # seaborn: statistical data visualization
@@ -32,4 +43,39 @@
 
 # Replacement for deprecated tsplot (not used anymore)
     # Timeseries plot with error bands : https://seaborn.pydata.org/examples/errorband_lineplots.html
-  
+
+
+# introduction to seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# plot shown in Jupyter Notebook
+%matplotlib inline  
+# expand the figure-width
+plt.rcParams['figure.figsize'] = 8, 4
+
+# To remove 'warnings' use 'filterwarnings'
+import warnings
+warnings.filterwarnings('ignore')
+
+
+
+
+
+# Visualize 'Distribution'
+    # practice with live-data
+# we'll use distplot(), 
+    # there are some important ploot functions like: 
+        # barplot(), boxplot()
+        # FacetGrid(), PairGrid(), clustermap(), 
+
+viz_1 = sns.distplot(stats.InternetUsers)
+# Note: This function has been deprecated and will be removed in seaborn v0.14.0. 
+    # It has been replaced by 'histplot' and 'displot', two functions with a modern API and many more capabilities.
+
+# More Modification. e.g. 'increase bars'
+    # use 'bins', 'hist'
+viz_1 = sns.distplot(stats.InternetUsers, bins=30)
+
+
+# --------    Boxplots    --------
