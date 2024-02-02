804cb7851e24318980d3ce056ff9064083b01c87
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-02-02 13:03:35 +0600

    Update pyDS_ch4_3_Subplots_KDE.py

--- py4DS_ch_4_VIZ_p1/pyDS_ch4_3_Subplots_KDE.py
+++ py4DS_ch_4_VIZ_p1/pyDS_ch4_3_Subplots_KDE.py
@@ -1,9 +1,9 @@
 
-################# 6.5: 2:50
+################# 6.5: 4:10
 # copy:  
 #        
 #        
-################# (30-jan-24 for 31-jan-24)
+################# (31-jan-24 for 2-feb-24)
 
 # Courses: A-Z PY for Data-Science    6.5, 6.6
 
@@ -55,12 +55,33 @@ vis1_sctr = sns.scatterplot(data=movies, x='CriticRating', y='AudienceRating')
 # lmplot: recall 'pyDS_ch2_8_HW_wrldTrnd.py'
 plt.style.use("seaborn")    # background color
 vis2_lm = sns.lmplot(data=movies, x='CriticRating', y='AudienceRating', fit_reg=False, hue='Genre', height=6, aspect=1, scatter_kws={'s': 10})
+# setting x,y axis range
+# vis2_lm.set(xlim=(-20, None))
+vis2_lm.set(xlim=(-20, 120), ylim=(-10, 110))
+
 
 # What are other way to visualize, other than 'scatter-plot' or 'lmplot'
     # To get more plot: Seaborn Gallery
 
 # KDE: Kernel Density Estimate
+kDe1 = sns.kdeplot(x=movies.CriticRating, y=movies.AudienceRating)  # this will get us a KDE-plot
+# it's pretty much the same as 'scatter-plot' or 'lmplot' but instead of dots we get the 'kernel-density-estimate'
+    # 'kernel-density-estimate' shows us most 'density of the data'
+    # how the density is distributed across the chart
+    # further away the 'kernel' the density gets lower (fading away)
 
+# why KDE used:
+    # to get the idea 'where two data are mostly correlated'
+    # how data is distributed accross the two-variables for a 'Bi-variate distribution'
 
+# ----  rev  ----
+# More Adjustments for the KDE-plot
+kDe2 = sns.kdeplot(x=movies.CriticRating, y=movies.AudienceRating, shade=True, shade_lowest=False)
+kDe3 = sns.kdeplot(x=movies.CriticRating, y=movies.AudienceRating, shade=True, shade_lowest=False, cmap='Reds')
+kDe4 = sns.kdeplot(x=movies.CriticRating, y=movies.AudienceRating, cmap='Reds')
+
+# combine with border
+kDe5 = sns.kdeplot(x=movies.CriticRating, y=movies.AudienceRating, shade=True, shade_lowest=False, cmap='Reds')
+kDe6 = sns.kdeplot(x=movies.CriticRating, y=movies.AudienceRating, cmap='Reds') # ads border on top of 'kDe5'
 
 
