431982d1f520372775415a2a4873d2f999d31f13
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-02-21 09:15:08 +0600

    Update pyDS_ch4_6_Dshbrd_styly_finalTuch.py

--- py4DS_ch_4_VIZ_p1/pyDS_ch4_6_Dshbrd_styly_finalTuch.py
+++ py4DS_ch_4_VIZ_p1/pyDS_ch4_6_Dshbrd_styly_finalTuch.py
@@ -1,8 +1,60 @@
 
-################# 348: FULL
+################# 6.11: 3.40
 # copy:  
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (20-feb-24 for 21-feb-24)
 
-# Courses: A-Z PY for Data-Science    0
+# Courses: A-Z PY for Data-Science    6.11, 6.12, 6.13
+
+
+# ------------    Load Dataset & Libraries    ------------
+# Import the following packages needed to perform the analysis
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# To remove 'warnings' use 'filterwarnings'
+import warnings
+warnings.filterwarnings('ignore')
+
+# plot shown in Jupyter Notebook
+%matplotlib inline  
+# expand the figure-width
+plt.rcParams['figure.figsize'] = 8, 4
+
+
+# Loading Dataset
+# Import the csv dataset
+movies = pd.read_csv("./MovieRatings.csv")     # load datset
+
+# -=-=-  Explore the data  -=-=-
+# Visualize the dataframe
+movies
+
+# rename the column names to single-string names
+movies.columns = ['Film', 'Genre', 'CriticRating', 'AudienceRating', 'BudgetMillions', 'Year']
+movies.head()    # view dataset
+
+
+# ----   convert 'numerical-type',' object-type' to "categorical-type"   ----
+movies.Film = movies.Film.astype("category") # use assignment '=' to update the dataset
+movies.Genre = movies.Genre.astype("category")   
+movies.Year = movies.Year.astype("category")   
+movies.info()   # check the data-type changes
+
+
+
+
+# ------------    Dashboard    ------------
+# What is a dashboard?
+    # a dasboard is a combination of charts
+    # for example sub-plot we built previously is a king of dashboard
+    # because it combines two types of plot with different informations
+
+# In this section we'll use "subplot" to create a 'Dashboard'
+    # we'll populate those dashboard with different types of chart
+    # we'll use a 2x2 subplot
+
+sns.set_style('darkgrid')   # change style
+dshBd, axes = plt.subplots(2, 2, figsize = (15, 15))
