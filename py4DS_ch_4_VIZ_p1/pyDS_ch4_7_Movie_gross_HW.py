6108f096b44f104c4dd0696b84110e94e3e6b9f1
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-03-09 06:09:50 +0600

    Update pyDS_ch4_7_Movie_gross_HW.py

--- py4DS_ch_4_VIZ_p1/pyDS_ch4_7_Movie_gross_HW.py
+++ py4DS_ch_4_VIZ_p1/pyDS_ch4_7_Movie_gross_HW.py
@@ -1,8 +1,41 @@
 
-################# 348: FULL
-# copy:  
+################# 6.14
+# copy: dataset, initial files etc
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (8-mar-24 for 9-mar-24)
 
-# Courses: A-Z PY for Data-Science    0
+# Courses: A-Z PY for Data-Science    6.14, 6.15
+
+
+
+
+
+# ------------    SECTION RECAP    --------
+""" 
+In this section we learned:
+1.	Category data type in Python
+    what wev'e learned:
+    
+2.	JointPlots
+3.	Histograms
+4.	Stacked Histograms
+5.	KDE Plot
+6.	Using the Subplots() function
+7.	Violinplots
+8.	Creating a Facet Grid
+9.	Coordinates and Diagonals
+10.	Building Dashboards
+11.	Styling Tips
+12.	Finishing Touches
+
+ """
+
+
+
+
+# ------------    HOMEWORK Movie Domestic Gross    --------
+
+
+
+
