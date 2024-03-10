18ae4609cc85f931913d63b2cd403d7d3a9b33c4
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-03-10 15:54:00 +0600

    Update pyDS_ch4_7_Movie_gross_HW.py

--- py4DS_ch_4_VIZ_p1/pyDS_ch4_7_Movie_gross_HW.py
+++ py4DS_ch_4_VIZ_p1/pyDS_ch4_7_Movie_gross_HW.py
@@ -1,40 +1,63 @@
 
-################# 6.14
+################# 6.14: full
 # copy: dataset, initial files etc
 #        
 #        
-################# (8-mar-24 for 9-mar-24)
+################# (9-mar-24 for 10-mar-24)
 
-# Courses: A-Z PY for Data-Science    6.14, 6.15
-
-
+# Courses: A-Z PY for Data-Science    6.14, 6.15, 7.7, 7.8, 7.9
 
 
 
-# ------------    SECTION RECAP    --------
+
+# ------------    SECTION RECAP    ------------
 """ 
-In this section we learned:
-1.	Category data type in Python
-    what wev'e learned:
-    
-2.	JointPlots
-3.	Histograms
-4.	Stacked Histograms
-5.	KDE Plot
-6.	Using the Subplots() function
-7.	Violinplots
-8.	Creating a Facet Grid
-9.	Coordinates and Diagonals
-10.	Building Dashboards
-11.	Styling Tips
-12.	Finishing Touches
+    In this section we learned:
 
- """
+        1.	Category data type in Python
+                * seperate 'numerical' data from 'categorical' data
+                * e.g. "Years" are categorical data (even though they are numbers)
+                * we converted "Years" numeric to categorical
+            
+        2.	JointPlots
+                * Hex type of jointplot, kind='hex', 'Hexagonal' clustered presentation
+
+        3.	Histograms
+
+        4.	Stacked Histograms
+                * used python to plot using 'pyplot'
+
+        5.	KDE Plot
+                * we mostly used
+
+        6.	Using the Subplots() function
+                * layout/figure containing multiple plots
+
+        7.	Violinplots
+                * comparison between 'Violinplots' & 'Boxplot'
+
+        8.	Creating a Facet Grid
+                * observe evolution of a dataset in a plot
+
+        9.	Coordinates and Diagonals
+                * setting facet-grids axix & draw diagonal for comparison
+
+        10.	Building Dashboards
+                * using 'subplots' with 'seaborn'
+                * overlaying plots on top of other
+
+        11.	Styling Tips
+                * background, seaborn styles
+
+        12.	Finishing Touches
+                * Thematic changes, title, axis-lables, font-sizes, tick-font-size, legend size
+
+"""
 
 
 
 
-# ------------    HOMEWORK Movie Domestic Gross    --------
+# ------------    Movie Domestic Gross (HOMEWORK)    ------------
 
 
 
