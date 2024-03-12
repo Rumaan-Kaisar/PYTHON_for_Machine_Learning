3907ad206f8fc2d9cdc081ed87b72640d2ebfe49
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-03-12 12:33:54 +0600

    Update pyDS_ch4_7_Movie_gross_HW.py

--- py4DS_ch_4_VIZ_p1/pyDS_ch4_7_Movie_gross_HW.py
+++ py4DS_ch_4_VIZ_p1/pyDS_ch4_7_Movie_gross_HW.py
@@ -1,9 +1,9 @@
 
-################# 6.14: full
-# copy: dataset, initial files etc
+################# 6.14: full, 6.15: 3.50
+# copy: dataset, pevious_chart, fill_in_blank.py
 #        
 #        
-################# (9-mar-24 for 10-mar-24)
+################# (10-mar-24 for 12-mar-24)
 
 # Courses: A-Z PY for Data-Science    6.14, 6.15, 7.7, 7.8, 7.9
 
@@ -57,8 +57,48 @@
 
 
 
-# ------------    Movie Domestic Gross (HOMEWORK)    ------------
+# ------------    Movie % Domestic Gross (HOMEWORK)    ------------
+""" 
+    The movie reviews website was very happy with your deliverable for the previous assignment (visualizations in this section, e.g. dashboard).
+    
+    Now they have a new request for you:
+        The previous consultant had created a chart for them which is illustrated on the given image. 
+        However the 'Python code' used to create the diagram has since been lost and cannot be recovered (LOL). 
+
+    1. Your task is to come up with the code that will  re-create the same chart making it look as close as possible to the original.
+
+    2. Also A new dataset has been supplied.
 
 
 
+    --------    Dataset    --------
+    Dataset: Following parameters are important for analysis.
+        There are 608 data/entries
 
+        Day of the week: the day movie was launched.
+        Director
+        Genre: action, comedy etc.
+        Movie Title
+        Relese date
+        Studio
+
+        Gross in milin dollers
+        Adjusted gross: Because some Movies relesed in the previous years
+                        Adjusted for the inflations
+        Budget in milin dollers
+
+        IMDB rating
+        movieLens rating
+
+        Oveseas gross: Earnings outside the US; 
+        overseas %: for comparison with total gross
+        Profit: million dollers (from budget and the gross)
+        Profit %
+        Runtime: length of the movie
+        US gross & US gross %
+
+
+
+    --------    Previous plot    --------
+
+"""
