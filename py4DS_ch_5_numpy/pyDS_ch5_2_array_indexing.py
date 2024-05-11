89c49f8c4fd06efd40a0162974b78c0f936ba32d
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-05-12 05:39:30 +0600

    Update pyDS_ch5_2_array_indexing.py

--- py4DS_ch_5_numpy/pyDS_ch5_2_array_indexing.py
+++ py4DS_ch_5_numpy/pyDS_ch5_2_array_indexing.py
@@ -1,5 +1,5 @@
 
-################# 5.4
+################# 5.4: 3:40
 # copy:  
 #        
 #        
@@ -13,16 +13,32 @@
 # we'll see how to index and slice numpy arrays
     # it works exactly like a normal Python list
 
+# Objective: How to select elements or group certain elements from a Numpy array
+
+import numpy as np
+
+arr_1 = np.arrange(11) # numpy array of 11 elements
+arr_1
+
+# we'll use [], and slice using ':' just like python normal list
+arr_1[10]   # gets the value at index 10
+
+# use slice notation [start(inclusive) : stop(exclusive)]
+arr_1[1:5]
+arr_1[0:6]
+
+# everything UPTO 
+arr_1[:6]   # same as arr_1[0:6]
+
+# start and UPTO END [start(inclusive) : ]
+arr_1[6:]
 
 
+
+# numpy Array differ from python list:
 """ 
-
-Just a quick note on the upcoming lecture I create a numpy array that goes from 0 to 10, and I accidentally use the word "index" when I should have said "position".
-
-I misspoke at two timestamps in the video- at 2:51 I say "5" is at index 6, while it's actually at index 5. What I meant to say is it is the 6th element in the array, but it is located at index 5 (just like a normal Python list, indexing starts at zero in Numpy arrays). Therefore, the arr[5:] notation actually grabs everything starting with the index 5, not everything beyond index 5. I then repeat the same thing at 3:21 by saying index 5 is at 4, what I should have said was the element 4 is the 5th element in the array, but it is still at index 4. 
-
-you should be able to watch the video lecture and clearly see how to index and slice arrays, it works exactly like a normal Python list.
-
+    Just like a "normal Python list", indexing starts at zero in Numpy arrays. 
+        Therefore, the arr[5:] notation actually grabs everything starting with the index 5.
 """
 
 
