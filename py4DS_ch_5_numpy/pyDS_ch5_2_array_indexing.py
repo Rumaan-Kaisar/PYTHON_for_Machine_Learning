22cbad1eb3afa2898974718a5d2da8be32a5addf
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-05-11 08:55:00 +0600

    Update pyDS_ch5_2_array_indexing.py

--- py4DS_ch_5_numpy/pyDS_ch5_2_array_indexing.py
+++ py4DS_ch_5_numpy/pyDS_ch5_2_array_indexing.py
@@ -1,8 +1,28 @@
 
-################# 348: FULL
+################# 5.4
 # copy:  
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (8-may-24 for 10-may-24)
 
-# Courses: A-Z PY for Data-Science    0
+# Courses: 
+    # PrTla PY for DS & ML : 5.4, 5.5
+
+
+# --------    numpy array indexing    --------
+# we'll see how to index and slice numpy arrays
+    # it works exactly like a normal Python list
+
+
+
+""" 
+
+Just a quick note on the upcoming lecture I create a numpy array that goes from 0 to 10, and I accidentally use the word "index" when I should have said "position".
+
+I misspoke at two timestamps in the video- at 2:51 I say "5" is at index 6, while it's actually at index 5. What I meant to say is it is the 6th element in the array, but it is located at index 5 (just like a normal Python list, indexing starts at zero in Numpy arrays). Therefore, the arr[5:] notation actually grabs everything starting with the index 5, not everything beyond index 5. I then repeat the same thing at 3:21 by saying index 5 is at 4, what I should have said was the element 4 is the 5th element in the array, but it is still at index 4. 
+
+you should be able to watch the video lecture and clearly see how to index and slice arrays, it works exactly like a normal Python list.
+
+"""
+
+
