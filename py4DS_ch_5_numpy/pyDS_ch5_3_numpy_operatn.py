548a5c5f77ccbddca2f98259e77bf725ad746081
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-05-21 17:43:30 +0600

    Update pyDS_ch5_3_numpy_operatn.py

--- py4DS_ch_5_numpy/pyDS_ch5_3_numpy_operatn.py
+++ py4DS_ch_5_numpy/pyDS_ch5_3_numpy_operatn.py
@@ -1,8 +1,14 @@
 
-################# 348: FULL
+################# 5.6, 5.7, 5.8
 # copy:  
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (19-May-24 for 21-May-24)
 
-# Courses: A-Z PY for Data-Science    0
+# Courses: 
+    # PrTla PY for DS & ML : 5.6, 5.7(ex), 5.8(ex-soln) 
+
+
+# ------------    numpy OPERATIONS    ------------
+
+
