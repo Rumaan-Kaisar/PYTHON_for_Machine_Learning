c6e4264d956da399ca5f0683479e3fc6e7db627f
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-05-22 13:07:46 +0600

    Update pyDS_ch5_3_numpy_operatn.py

--- py4DS_ch_5_numpy/pyDS_ch5_3_numpy_operatn.py
+++ py4DS_ch_5_numpy/pyDS_ch5_3_numpy_operatn.py
@@ -1,14 +1,28 @@
 
-################# 5.6, 5.7, 5.8
+################# 5.6: 1.10
 # copy:  
 #        
 #        
-################# (19-May-24 for 21-May-24)
+################# (21-May-24 for 22-May-24)
 
 # Courses: 
     # PrTla PY for DS & ML : 5.6, 5.7(ex), 5.8(ex-soln) 
 
 
 # ------------    numpy OPERATIONS    ------------
+# Basic operations we perform on numpy array
+    # Array with Array 
+    # Array with Scalars 
+    # Universal Array Functions
+
+import numpy as np
+arr = np.arange(0,11)
+print(arr)
+
+
+# ------    Array with Array    ------
+# we can do simple arithmetic operation with two array
+# following adds "element-by-element" basis
+arr + arr
 
 
