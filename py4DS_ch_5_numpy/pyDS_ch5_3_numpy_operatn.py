0154f91d6a7e27b31a2447ad8b193185a3b7accc
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-05-24 05:54:51 +0600

    Update pyDS_ch5_3_numpy_operatn.py

--- py4DS_ch_5_numpy/pyDS_ch5_3_numpy_operatn.py
+++ py4DS_ch_5_numpy/pyDS_ch5_3_numpy_operatn.py
@@ -1,9 +1,9 @@
 
-################# 5.6: 1.10
+################# 5.6: 3.41
 # copy:  
 #        
 #        
-################# (21-May-24 for 22-May-24)
+################# (22-May-24 for 24-May-24)
 
 # Courses: 
     # PrTla PY for DS & ML : 5.6, 5.7(ex), 5.8(ex-soln) 
@@ -25,4 +25,32 @@ print(arr)
 # following adds "element-by-element" basis
 arr + arr
 
+# similarly we can do subtraction and multiplication
+arr - arr
+arr * arr
+
+# ------    scaler operation    ------
+# numpy basically broadcast the scaler to every element 
+# the operation occured in also "element by element"
+
+arr + 100
+arr / 100
+arr * 100
+
+
+# --------    numpy warning    --------
+# dividing elements by 0 cause ERROR in "python"
+# but "numpy" gives a warning instead of ERROR
+1/0     # gives ERROR in python: ZeroDivisionError
+
+# numpy gives an "warning" but no ERROR
+    # notice the first element of the "arr" is 0 and 
+    # 0/0 will give a ERROR in python
+    # but in "numpy" we'll get an warning instead
+        # it will return "nan" for 0/0 in numpy
+
+arr / arr       # Runtimewarning: invalid value encountered in true_divide
+
+# following returns 'inf' (infinity) for the first element when performing '1/0'
+1 / arr
 
