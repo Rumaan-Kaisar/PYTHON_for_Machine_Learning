0325904085182e5b6ef1d6902d83308e201d33c1
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-05-25 09:42:55 +0600

    Update pyDS_ch5_3_numpy_operatn.py

--- py4DS_ch_5_numpy/pyDS_ch5_3_numpy_operatn.py
+++ py4DS_ch_5_numpy/pyDS_ch5_3_numpy_operatn.py
@@ -1,9 +1,9 @@
 
-################# 5.6: 3.41
+################# 5.6: 5.28
 # copy:  
 #        
 #        
-################# (22-May-24 for 24-May-24)
+################# (24-May-24 for 25-May-24)
 
 # Courses: 
     # PrTla PY for DS & ML : 5.6, 5.7(ex), 5.8(ex-soln) 
@@ -28,6 +28,7 @@ arr + arr
 # similarly we can do subtraction and multiplication
 arr - arr
 arr * arr
+arr**2      # exponents
 
 # ------    scaler operation    ------
 # numpy basically broadcast the scaler to every element 
@@ -41,6 +42,7 @@ arr * 100
 # --------    numpy warning    --------
 # dividing elements by 0 cause ERROR in "python"
 # but "numpy" gives a warning instead of ERROR
+# so be careful about having 0 in your dataset
 1/0     # gives ERROR in python: ZeroDivisionError
 
 # numpy gives an "warning" but no ERROR
@@ -53,4 +55,22 @@ arr / arr       # Runtimewarning: invalid value encountered in true_divide
 
 # following returns 'inf' (infinity) for the first element when performing '1/0'
 1 / arr
+
+
+# --------    universal array function    --------
+# universal array functions are just mathematical operations
+# perform-operation & broadcast over array
+np.sqrt(arr)    # square root
+np.exp(arr)     # e^n : calculates exponent e**n for each array element
+
+np.max(arr)     # maximum using numpy method
+# alternatively
+arr.max()       # maximum using builtin array method/property
+
+# ----  trigonometric functions  ----
+np.sin(arr)     # gets the sin value element-wise
+
+# ----  logarithmic value  ----
+# NOTE: for 0, we'll get "-ve inf" and a RuntimeWarning for "divide by 0"
+np.log(arr)     # gets the "e"-based-log, i.e. ln() value element-wise
 
