c0de851bcc95b507039a1094fe486a68082023ef
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-06-11 11:03:16 +0600

    Update pyDS_ch6_1_series.py

--- py4DS_ch_6_pandas/pyDS_ch6_1_series.py
+++ py4DS_ch_6_pandas/pyDS_ch6_1_series.py
@@ -1,11 +1,32 @@
 
-################# 348: FULL
+################# 6.1: FULL, 6.2: 0.52
 # copy:  
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (09-jun-24 for 11-jun-24)
 
 # Courses:  PrTla PY for DS & ML >   6.1, 6.2, 6.3
 #           Previously: py4DS_ch_2_dataFrame 
+
+# ------------    pandas    ------------
+# pandas is a library for datascience tools
+    # it is used for data analysis and manipulation
+    # it provides data structures like "DataFrame" and "Series" for handling and analyzing structured data.
+
+# we can call it: python version of excel or python vesrsion of R
+
+# Pandas is an open source library built on top of "NumPy" 
+    # i.e. you need to learn numpy first
+
+    # It allows for :
+            # fast analysis 
+            # data cleaning  
+            # preparation
+
+    # It excels in performance and productivity
+    # It also has built-in data-visualization features
+    # It can work with data from a wide variety of sources.
+
+# install: 
 
 
