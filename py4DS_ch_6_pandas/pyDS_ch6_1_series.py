cbdfdc8a9747b82ba951639e85a8f9c775ee4240
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-06-12 15:09:17 +0600

    Update pyDS_ch6_1_series.py

--- py4DS_ch_6_pandas/pyDS_ch6_1_series.py
+++ py4DS_ch_6_pandas/pyDS_ch6_1_series.py
@@ -1,5 +1,5 @@
 
-################# 6.1: FULL, 6.2: 0.52
+################# 6.1: FULL, 6.2: full, 6.3: 0.34
 # copy:  
 #        
 #        
@@ -7,6 +7,8 @@
 
 # Courses:  PrTla PY for DS & ML >   6.1, 6.2, 6.3
 #           Previously: py4DS_ch_2_dataFrame 
+
+
 
 # ------------    pandas    ------------
 # pandas is a library for datascience tools
@@ -28,5 +30,29 @@
     # It can work with data from a wide variety of sources.
 
 # install: 
+    # open cmd          >>  pip install pandas
+    # or open conda     >>  conda install pandas
 
 
+# --------   Objectives   --------
+    # Series 
+    # DataFrames 
+    # Missing Data 
+    # GroupBy
+    # Mergingjoining^nd Concatenating 
+    # Operations
+    # Data Input and Output: csv, excel files, SQL and more
+
+
+
+# --------    series    --------
+# it's the one of the "Pandas Data-type"
+    # we'll use series a lot to build Data-Frames
+
+# A "series" is very similar to "numpy-array"
+    # it's actually built on top of "numpy-array object"
+
+# The difference bewteen "pandas-series" and "numpy-array" is:
+    # series can access lebels, i.e. it can "indexed by labels"
+
+    
