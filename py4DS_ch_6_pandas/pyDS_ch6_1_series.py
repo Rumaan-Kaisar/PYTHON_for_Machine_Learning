680be0708feee726978c3e33fe68ac333f293d01
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-06-16 12:03:01 +0600

    Update pyDS_ch6_1_series.py

--- py4DS_ch_6_pandas/pyDS_ch6_1_series.py
+++ py4DS_ch_6_pandas/pyDS_ch6_1_series.py
@@ -1,10 +1,4 @@
 
-################# 6.1: FULL, 6.2: full, 6.3: 5:44
-# copy:  
-#        
-#        
-################# (14-jun-24 for 15-jun-24)
-
 # Courses:  PrTla PY for DS & ML >   6.1, 6.2, 6.3
 #           Previously: py4DS_ch_2_dataFrame 
 
@@ -48,6 +42,7 @@
 # --------    series    --------
 # it's the one of the "Pandas Data-type"
     # we'll use series a lot to build Data-Frames
+    # but we mostly use DataFrames instead of Series
 
 # A "series" is very similar to "numpy-array"
     # it's actually built on top of "numpy-array object"
@@ -109,5 +104,30 @@ pd.Series(data= laBls)
 pd.Series(data= [sum, avg, max, min], index=["s", "a", "mx", "mn"])
 
 
+
+# ----  using index of a series  ----
+    # The key to using a Series is: understanding its index.
+    # it works like a "HASH-table" or a "dictionary"
+
+# grab information from a Series.
+ser_1 = pd.Series(data=[1,2,3,4], index=['USA', 'Germany', 'USSR','Japan'])
+ser_2 = pd.Series(data=[1,2,5,4], index=['USA', 'Germany', 'Italy','Japan'])
+ser_3 = pd.Series(data= laBls)
+# dtype shows the type of the data: "int64" for integers, "object" for strings and others
+
+# accessing data: specify the index labels
+ser_1['USA']
+ser_3[2]
+
+
+# basic operations are done w.r. to "index"
+# addition:
+    # try to match-up the operation based on the undex
+    # eg: 'USA'  matched and thus it will be 2
+    # "NaN" is used for non-matched items. eg: 'USSR' & 'Italy'
+ser_1 + ser_2 
+
+# Note: "int" will convert to "float" Working with NumPy or Pandas based object
+#           it's done to retain all the information. eg: int-division data loss
 
 
