c4d554cb1824b2abcd48474faf3d47570325e27a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-06-19 14:26:09 +0600

    Update pyDS_ch6_2_dataFrame_basic.py

--- py4DS_ch_6_pandas/pyDS_ch6_2_dataFrame_basic.py
+++ py4DS_ch_6_pandas/pyDS_ch6_2_dataFrame_basic.py
@@ -1,9 +1,9 @@
 
-################# 6.4: 1.19
+################# 6.4: 3.50
 # copy:  
 #        
 #        
-################# (16-jun-24 for 18-jun-24 )
+################# (18-jun-24 for 19-jun-24 )
 
 # Courses: PrTla PY for DS & ML >   6.4, 6.5(1/2)
 
@@ -24,16 +24,6 @@ from numpy.random import randn
 np.random.seed(101)
 rnd_20 = randn(5, 4)
 rnd_20
-
-# --------    Building a Dataframe    --------
-    # similar to Series(), DataFrame() also takes, "data", "index" as arguments
-    # there ia also a "columns" argumnet for columns
-    # note that: "index" also represents the rows
-
-fd = pd.DataFrame(data=rnd_20, index=["r1", "r2", "r3", "r4", "r5"], columns=["c1", "c2", "c3", "c4"])
-# randn(5, 4) generates 5x4 matrix of 20 random numbers from "NORMAL distribution"
-    # that's why we've used "5-index for 5-rows" and 4-columns
-
 
 
 
@@ -56,6 +46,34 @@ np.random.seed(42)
 random_numbers = np.random.rand(5)
 
 print(random_numbers)
+
+
+
+
+# --------    Building a Dataframe    --------
+    # similar to Series(), DataFrame() also takes, "data", "index" as arguments
+    # there ia also a "columns" argumnet for columns
+    # note that: "index" also represents the rows
+
+fd = pd.DataFrame(data=rnd_20, index=["r1", "r2", "r3", "r4", "r5"], columns=["c1", "c2", "c3", "c4"])
+# randn(5, 4) generates 5x4 matrix of 20 random numbers from "NORMAL distribution"
+    # that's why we've used "5-index for 5-rows" and 4-columns
+
+# Each column is a "Pandas-Series", so, "c1" is a Series, as well as "c2", "c3", "c4"
+# and they all share a "Common-index"
+
+# Basically all DataFrames is a bunch of series that shares a same index
+# we can select these Series-objects
+
+# --------    accessing Series from DataFrame    --------
+# method 1: specify the column name
+fd['c3']
+# it is actually a Series, we can confirm it by type checking
+type(fd['c3'])
+# but the type of 'fd' is DataFrame
+type(fd)
+
+# method 2: SQL-format, useing "."
 
 
 
