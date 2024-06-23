4547f26643ebdf00c949f81d59844588db225ae4
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-06-23 17:48:37 +0600

    Update pyDS_ch6_2_dataFrame_basic.py

--- py4DS_ch_6_pandas/pyDS_ch6_2_dataFrame_basic.py
+++ py4DS_ch_6_pandas/pyDS_ch6_2_dataFrame_basic.py
@@ -1,5 +1,5 @@
 
-################# 6.4: 12.7
+################# 6.4: full, 6.5: 0.53
 # copy:  
 #        
 #        
@@ -55,6 +55,7 @@ print(random_numbers)
     # there ia also a "columns" argumnet for columns
     # note that: "index" also represents the rows
 
+# ----  creating a DataFrame  ----
 fd = pd.DataFrame(data=rnd_20, index=["r1", "r2", "r3", "r4", "r5"], columns=["c1", "c2", "c3", "c4"])
 # randn(5, 4) generates 5x4 matrix of 20 random numbers from "NORMAL distribution"
     # that's why we've used "5-index for 5-rows" and 4-columns
@@ -65,10 +66,12 @@ fd = pd.DataFrame(data=rnd_20, index=["r1", "r2", "r3", "r4", "r5"], columns=["c
 # Basically all DataFrames is a bunch of series that shares a same index
 # we can select these Series-objects
 
-# --------    accessing Series from DataFrame    --------
+
+
+# --------    accessing Series from DataFrame: acessing columns and rows    --------
 # method 1: specify the column name
 fd['c3']
-# it is actually a Series, we can confirm it by type checking
+# it is actually a "Series", we can confirm it by type checking
 type(fd['c3'])
 # but the type of 'fd' is DataFrame
 type(fd)
@@ -85,14 +88,19 @@ print(fd.c3)
     # single column: Series, multi-column: DataFrame
 fd[['c2', 'c4']]
 
-# creating a new column
+
+
+# ----  creating a new column  ----
 fd['new1'] = [113, 123, 133, 143, 153]
 
 # or we can use operations
 fd['new2'] = fd['c1'] + fd['c2']
 fd
 
-# DELETING a column
+
+
+# ----  DELETING a column  ----
+# use "inplace" to make the change permanenet
 # axis: 0 is row, 1 is column
 fd.drop('nwe1', axis=1)
 
@@ -103,7 +111,7 @@ print(fd)
 fd.drop('nwe1', axis=1, inplace=True)
 print(fd)
 
-# drop a row
+# drop a row, axis = 0
 fd.drop('r2')   # by default axis=0
 # fd.drop('r2', axis=0)
 
@@ -111,6 +119,7 @@ fd.drop('r2')   # by default axis=0
     # if we notice the shape of the DataFrame
 fd.shape    # (row, column)
     # (row, column) is a tuple, row at index 0, and column is index 1
+
 
 
 # --------  selecting rows: loc[] and iloc[]  --------
@@ -130,5 +139,21 @@ fd.iloc[1]    # accessing row 2
 
 
 # ------------    subsetting: sub-sets of rows and columns    ------------
+# use loc[row, column]
+    # "row", "column" can be lables or 'list of labels'
+
+# single value at row "r2" and at column "c3"
+fd.loc['r2', 'c3']
+
+# subset of valuees: use lists of "row", "column"
+    #  getting values from r1, r3 rows and c2, c4 columns
+fd.loc[['r1', 'r3'], ['c2', 'c4']]
 
 
+
+
+# ------------    conditional selections and multi index    ------------
+
+# ------    conditional selections    ------
+# we can apply conditional selection using brackets notation in pandas
+# it is very similar to NumPy
