67eb181f17a554b035f0d5d6403ad33388b27acc
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-07-05 16:10:09 +0600

    Update pyDS_ch6_3_dataFrame_dtail.py

--- py4DS_ch_6_pandas/pyDS_ch6_3_dataFrame_dtail.py
+++ py4DS_ch_6_pandas/pyDS_ch6_3_dataFrame_dtail.py
@@ -1,9 +1,9 @@
 
-################# 6.5: full, 6.6: 0.48
-# copy:  ipynbs from PrTla
+################# 6.5: full, 6.6: 1.04
+# copy:  
 #        
 #        
-################# (02-jul-24 for 03-jul-24)
+################# (03-jul-24 for 05-jul-24)
 
 # Courses: PrTla PY for DS & ML >   6.5(11:40), 6.6, 6.7
 
@@ -59,6 +59,53 @@ fd.set_index("States")  # overrides the old-index
 
 # --------    multi-index    --------
 # multi-index DataFrame
+import numpy as np
+import pandas as pd
+
+# two lists
+# "Gl G1 G1 G2 G2 G2".split()
+outside = ['Gl', 'G1', 'G1', 'G2', 'G2', 'G2']
+inside = [1,2,3,1,2,3]
+
+# create a "list of tuples / pairs"
+hier_index = list(zip(outside,inside))  # zip is used to create pair (a, b)
+hier_index  # [('Gl', 1), ('G1', 2), ('G1', 3), ('G2', 1), ('G2', 2), ('G2', 3)]
+
+# creating "Multi-index"
+# "pd.Multiindex.from_tuples" creates a multi-index from a "list of tuples"
+hier_index = pd.Multiindex.from_tuples(hier_index)
+hier_index
+
+
+
+# ---- rev[03-jul-2024: Update the code, not giving same output] ----
+
+
+# create the DataFrame
+
+# GPT:
+# Multi-index DataFrame
+import numpy as np
+import pandas as pd
+
+# Two lists
+outside = ['G1', 'G1', 'G1', 'G2', 'G2', 'G2']
+inside = [1, 2, 3, 1, 2, 3]
+
+# Create a "list of tuples / pairs"
+hier_index = list(zip(outside, inside))  # zip is used to create pairs (a, b)
+print(hier_index)  # [('G1', 1), ('G1', 2), ('G1', 3), ('G2', 1), ('G2', 2), ('G2', 3)]
+
+# Creating "Multi-index"
+# "pd.MultiIndex.from_tuples" creates a multi-index from a "list of tuples"
+hier_index = pd.MultiIndex.from_tuples(hier_index)
+print(hier_index)
+
+# Create a DataFrame with the multi-index
+df = pd.DataFrame(np.random.randn(6, 2), index=hier_index, columns=['A', 'B'])
+print(df)
+
+
 
 # working with a multi-index DataFrame
 
