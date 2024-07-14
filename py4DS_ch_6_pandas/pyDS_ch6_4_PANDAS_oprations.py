1c55f2870865e62ac89ccc1f4b85a7a298096e38
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-07-14 14:15:02 +0600

    Update pyDS_ch6_4_PANDAS_oprations.py

--- py4DS_ch_6_pandas/pyDS_ch6_4_PANDAS_oprations.py
+++ py4DS_ch_6_pandas/pyDS_ch6_4_PANDAS_oprations.py
@@ -1,11 +1,16 @@
 
-################# 348: FULL
-# copy:  
+################# 348: 6.8:0.28
+# copy:  image
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (13-JUL-24 for 14-JUL-24)
 
 # Courses: PrTla PY for DS & ML >   6.8, 6.9, 6.10, 6.11
 
+
+# ------------    Groupby    ------------
+# Groupby allows you to "group together rows" based off of a column and perform an aggregate function on them.
+
+# Groupby and SQL:
 
 
