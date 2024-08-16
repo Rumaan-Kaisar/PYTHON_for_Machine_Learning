ae1c5c349e3515b744c3cb6f448ef444d70ca556
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-08-16 14:44:03 +0600

    Add files via upload

---
+++ py4DS_ch_6_pandas/pyDS_ch6_5_SFsalary_Ecommerce_HW.py
@@ -0,0 +1,19 @@
+
+
+################# 7.2
+# copy:
+#        
+#        
+################# (14-Aug-24 for 16-Aug-24)
+
+# Courses: PrTla PY for DS & ML >   7.2, 7.3, 7.4, 7.5
+
+
+""" 
+On the Salary Exercise, one of the questions asks how many people have the word "chief" in their title. My solution is actually slightly off, I didn't take into account people that had punctuation in their title right next to the word chief, for example : Chief, Justice. 
+
+If you want to see a full discussion, search the qa forums for 'chief' , there are a lot of thread where students have posted alternative solutions that work correctly (there are a lot of ways to solve this one)
+
+ """
+
+ 
