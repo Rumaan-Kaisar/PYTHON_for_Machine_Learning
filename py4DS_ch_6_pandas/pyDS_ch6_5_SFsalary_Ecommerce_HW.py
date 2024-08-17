820f7b74b76f1143219af061bd069b8d7ea34feb
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-08-17 12:46:03 +0600

    Update pyDS_ch6_5_SFsalary_Ecommerce_HW.py

--- py4DS_ch_6_pandas/pyDS_ch6_5_SFsalary_Ecommerce_HW.py
+++ py4DS_ch_6_pandas/pyDS_ch6_5_SFsalary_Ecommerce_HW.py
@@ -1,19 +1,28 @@
 
 
-################# 7.2
-# copy:
+################# 7.2: 0.39
+# copy: dataset-csv
 #        
 #        
-################# (14-Aug-24 for 16-Aug-24)
+################# (16-Aug-24 for 17-Aug-24)
 
 # Courses: PrTla PY for DS & ML >   7.2, 7.3, 7.4, 7.5
 
 
+
+# ------------    SF Salaries Overview    ------------
+
 """ 
-On the Salary Exercise, one of the questions asks how many people have the word "chief" in their title. My solution is actually slightly off, I didn't take into account people that had punctuation in their title right next to the word chief, for example : Chief, Justice. 
+On the Salary Exercise, one of the questions asks 
+    how many people have the word "chief" in their title. 
+    
+    The solution is actually slightly off, 
+    We didn't take into account people that had 
+        "punctuation" in their title right next to the word "chief", 
+        for example : Chief, Justice. 
+"""
 
-If you want to see a full discussion, search the qa forums for 'chief' , there are a lot of thread where students have posted alternative solutions that work correctly (there are a lot of ways to solve this one)
+# We will be using the SF Salaries Dataset: https://www.kaggle.com/kaggle/sf-salaries from Kaggle!
 
- """
 
- 
+
