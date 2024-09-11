6efa5441320b4e9e2effa2ad28d6deedbfc1256b
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-09-11 16:21:48 +0600

    Update pyDS_ch7_1_MatPltLib_intro.py

--- py4DS_ch_7_VIZ_p2_Matplotlib/pyDS_ch7_1_MatPltLib_intro.py
+++ py4DS_ch_7_VIZ_p2_Matplotlib/pyDS_ch7_1_MatPltLib_intro.py
@@ -1,8 +1,22 @@
 
-################# 348: FULL
+################# 8.1: FULL, 8.2: 0.52
 # copy:  
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (10-Sep-24 for 11-sep-24)
 
-# Courses: A-Z PY for Data-Science    0
+# Courses: PrTla PY for DS & ML >   8.1, 8.2
+
+
+# ----------------    matplotlib    ----------------
+# Since we're now fimiliar with DataFrames, how data is stored in that (and data-Analysis libraries), 
+# it's time to learn how can we visualize it
+
+# ----  Intro to Matplotlib  ----
+# Matplotlib is Python's most popular plotting library.
+# It allows full control over every part of a figure.
+# It was created to resemble MatLab's graphical plotting.
+# It works with Pandas and Numpy arrays 
+# 'Seaborn' (another plotting library) is built on top of 'Matplotlib'
+    # We need to understand Matplotlib first to learn Seaborn.
+
