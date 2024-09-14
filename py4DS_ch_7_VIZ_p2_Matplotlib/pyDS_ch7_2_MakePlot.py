66e5f473f4850955d75e31fdb9408a23cd374dee
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-09-14 14:02:58 +0600

    Update pyDS_ch7_2_MakePlot.py

--- py4DS_ch_7_VIZ_p2_Matplotlib/pyDS_ch7_2_MakePlot.py
+++ py4DS_ch_7_VIZ_p2_Matplotlib/pyDS_ch7_2_MakePlot.py
@@ -1,8 +1,49 @@
 
-################# 348: FULL
-# copy:  
+################# 8.3: FULL
+# copy:
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (13-Sep-24 for 14-sep-24)
 
-# Courses: A-Z PY for Data-Science    0
+# Courses: PrTla PY for DS & ML >   8.3, 8.4 (6.25)
+
+# ------------    Matplotlib concepts    ------------
+
+""" 
+
+Matplotlib is the main Python library for data visualization, created by "John Hunter" 
+    to replicate MatLab's plotting features in Python.
+
+If you're familiar with MatLab, Matplotlib will feel natural.
+
+It's a powerful 2D and 3D graphics library for scientific figures.
+
+
+Pros of Matplotlib:
+    - Easy to start with for simple plots
+    - Supports custom labels and text
+    - Full control over every element in a figure
+    - High-quality output in many formats
+    - Highly customizable
+
+Matplotlib lets you create reproducible figures programmatically. 
+You can explore more on the official Matplotlib website: http://matplotlib.org/.
+
+"""
+
+import matplotlib.pyplot as plt
+
+# shows plots directly in Jupyter notebooks.
+# If you're using another editor, add plt.show() at the end of your plotting commands 
+#   to display the figure in a separate window
+%matplotlib inline
+
+
+# 2 way to plot in Matplotlib:
+    # Functional way
+    # OOP way
+
+
+# --------    Functional way of plotting    --------
+
+
