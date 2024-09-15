4800d0c2cd886e6568aafe954bf0adb975730146
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-09-15 14:58:24 +0600

    Update pyDS_ch7_2_MakePlot.py

--- py4DS_ch_7_VIZ_p2_Matplotlib/pyDS_ch7_2_MakePlot.py
+++ py4DS_ch_7_VIZ_p2_Matplotlib/pyDS_ch7_2_MakePlot.py
@@ -1,9 +1,9 @@
 
-################# 8.3: FULL
+################# 8.3: 3.56
 # copy:
 #        
 #        
-################# (13-Sep-24 for 14-sep-24)
+################# (14-Sep-24 for 15-sep-24)
 
 # Courses: PrTla PY for DS & ML >   8.3, 8.4 (6.25)
 
@@ -34,9 +34,14 @@ You can explore more on the official Matplotlib website: http://matplotlib.org/.
 import matplotlib.pyplot as plt
 
 # shows plots directly in Jupyter notebooks.
+%matplotlib inline
 # If you're using another editor, add plt.show() at the end of your plotting commands 
 #   to display the figure in a separate window
-%matplotlib inline
+#   plt.show()    # other than Jupyter NB
+#   notice 'out' cell is gone when we use plt.show() in ipynb
+#   its similar to typing 'string' vs print('string')
+#       similarly plt.show() is printing the plot instead of showing the plot
+
 
 
 # 2 way to plot in Matplotlib:
@@ -45,5 +50,18 @@ import matplotlib.pyplot as plt
 
 
 # --------    Functional way of plotting    --------
+# MatLab syntax is also used, eg: to set color r
+import numpy as np
 
+# Let's define two numpy arrays. 
+# You can plot with lists, but you'll usually use numpy arrays or pandas columns, which act like arrays.
+
+# The data we want to plot:
+# linearly spaced 11 points between (0, 5)
+x = np.linspace(0, 5, 11)
+y = x**2    # output, function of x
+print(x)
+print(y)
+plt.plot(x, y)
+# plt.show()    # other than Jupyter NB
 
