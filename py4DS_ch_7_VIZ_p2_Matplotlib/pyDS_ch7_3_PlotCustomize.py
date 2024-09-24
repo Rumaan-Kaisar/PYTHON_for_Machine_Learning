636105819d421e797724a5bd1c319991cec98528
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-09-24 12:34:51 +0600

    Update pyDS_ch7_3_PlotCustomize.py

--- py4DS_ch_7_VIZ_p2_Matplotlib/pyDS_ch7_3_PlotCustomize.py
+++ py4DS_ch_7_VIZ_p2_Matplotlib/pyDS_ch7_3_PlotCustomize.py
@@ -1,8 +1,13 @@
 
-################# 348: FULL
+################# 8.4: 6.22
 # copy:  
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (10-Sep-24 for 11-sep-24)
 
-# Courses: A-Z PY for Data-Science    0
+# Courses: PrTla PY for DS & ML >   8.4 (6.22+), 8.5
+
+
+# --------    Figure size, aspect ratio and DPI    --------
+# by calling the 'fig' object we can specify those
+
