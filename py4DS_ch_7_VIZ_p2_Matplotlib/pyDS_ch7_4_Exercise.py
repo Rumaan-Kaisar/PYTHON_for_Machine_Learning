33742a7ddbabb06dcabd1c543ef3eed16d93c619
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-10-02 15:53:02 +0600

    Update pyDS_ch7_4_Exercise.py

--- py4DS_ch_7_VIZ_p2_Matplotlib/pyDS_ch7_4_Exercise.py
+++ py4DS_ch_7_VIZ_p2_Matplotlib/pyDS_ch7_4_Exercise.py
@@ -1,8 +1,20 @@
 
-################# 348: FULL
+################# 8.6: Full
 # copy:  
 #        
 #        
-################# (26-sep-23 for 27-sep-23)
+################# (10-Sep-24 for 11-sep-24)
 
-# Courses: A-Z PY for Data-Science    0
+# Courses: PrTla PY for DS & ML >   8.6, 8.7
+
+# ------------    matplotlib exercise    ------------
+# Following are reletively simple plots
+# Don't focus too much on the Matplotlib syntax; we'll be using Seaborn in the next chapter.
+# Since Seaborn is built on top of Matplotlib, it's important to understand Matplotlib first.
+
+# use following data
+import numpy as np
+x = np.arange(0, 100)   # array of [0, 1, ..., 99]
+y = 2*x
+z = x**2
+
