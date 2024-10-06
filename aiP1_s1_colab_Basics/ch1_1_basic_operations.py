a5a26acce45bf2a206b37a5c127dac08cfc5bbc2
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-10-06 21:16:32 +0600

    Add files via upload

---
+++ aiP1_s1_colab_Basics/ch1_1_basic_operations.py
@@ -0,0 +1,18 @@
+
+# codes from ----  rsk  ----
+
+# checking python
+!python --version
+!pip --version
+!pip list
+
+
+# install other versions for example:   python 3.5
+!sudo apt-get update -y
+!sudo apt-get install python3.5
+
+
+
+# --------  connect to Google drive  --------
+# Run the following code to mount your Google Drive to Colab:
+
