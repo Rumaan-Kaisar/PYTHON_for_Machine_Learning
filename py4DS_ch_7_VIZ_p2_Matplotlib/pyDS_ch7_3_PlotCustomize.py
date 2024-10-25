2f336f4adc4ad24e48ebd92808dfdf3eaf8c53e7
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-10-25 13:42:49 +0600

    Update pyDS_ch7_3_PlotCustomize.py

--- py4DS_ch_7_VIZ_p2_Matplotlib/pyDS_ch7_3_PlotCustomize.py
+++ py4DS_ch_7_VIZ_p2_Matplotlib/pyDS_ch7_3_PlotCustomize.py
@@ -792,8 +792,3 @@ axAdv21.set_zlim3d(-np.pi, 2*np.pi);
  """
 
 
-* http://www.matplotlib.org - The project web page for matplotlib.
-* https://github.com/matplotlib/matplotlib - The source code for matplotlib.
-* http://matplotlib.org/gallery.html - A large gallery showcaseing various types of plots matplotlib can create. Highly recommended! 
-* http://www.loria.fr/~rougier/teaching/matplotlib - A good matplotlib tutorial.
-* http://scipy-lectures.github.io/matplotlib/matplotlib.html - Another good matplotlib reference.
