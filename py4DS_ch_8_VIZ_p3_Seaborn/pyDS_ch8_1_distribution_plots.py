061fc6a20edb4068ff21b3b258a3567f827db13f
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-11-06 14:00:57 +0600

    Update pyDS_ch8_1_distribution_plots.py

--- py4DS_ch_8_VIZ_p3_Seaborn/pyDS_ch8_1_distribution_plots.py
+++ py4DS_ch_8_VIZ_p3_Seaborn/pyDS_ch8_1_distribution_plots.py
@@ -1,8 +1,26 @@
 
-################# 348: FULL
+################# 9.1
 # copy:  
 #        
 #        
 ################# (03-Nov-24 for 04-Nov-24)
 
 # Courses: PrTla PY for DS & ML >    9.1, 9.2
+
+
+# ------------    distribution plots    ------------
+
+# Let's go over some plots that help visualize the distribution of a dataset
+        #   distplot
+        #   jointplot
+        #   pairplot
+        #   rugplot
+        #   kdeplot
+
+# ----  rev [05-nov-2024]  ----
+
+
+
+
+
+
