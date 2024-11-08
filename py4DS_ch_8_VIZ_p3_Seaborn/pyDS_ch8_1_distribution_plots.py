1e3481d839b77497cd762b85b9f86041927f55fe
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-11-08 08:51:55 +0600

    Update pyDS_ch8_1_distribution_plots.py

--- py4DS_ch_8_VIZ_p3_Seaborn/pyDS_ch8_1_distribution_plots.py
+++ py4DS_ch_8_VIZ_p3_Seaborn/pyDS_ch8_1_distribution_plots.py
@@ -1,9 +1,9 @@
 
-################# 9.1
+################# 9.1: full.
 # copy:  
 #        
 #        
-################# (03-Nov-24 for 04-Nov-24)
+################# (06-Nov-24 for 07-Nov-24)
 
 # Courses: PrTla PY for DS & ML >    9.1, 9.2
 
@@ -19,7 +19,24 @@
 
 # ----  rev [05-nov-2024]  ----
 
+# what is seaborn:
+    # Seaborn is a statistical plotting library based on Matplotlib
+    # It has default styles that works with pandas DataFrame objects
+    # It has a wide range of plot types for visualizing 
+        # distributions, 
+        # relationships, and 
+        # categorical data.
 
+# how to install:
+    # conda install seaborn
+    # or
+    # pip install seaborn
+
+# documentation:
+    # GitHub: we can find it on GitHub since it's open-source:  https://github.com/mwaskom/seaborn
+    # official: https://seaborn.pydata.org/
+        # use "gallery" as reference, eg: heatmap, vilinplot, boxplot, joint plot
+        # API: reference for various plot-types
 
 
 
