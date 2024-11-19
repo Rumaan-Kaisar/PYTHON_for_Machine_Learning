f1e47e71f5c86b1962edf7b9c866a93dde43dfca
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-11-19 11:53:11 +0600

    Update pyDS_ch8_2_categorical_plots.py

--- py4DS_ch_8_VIZ_p3_Seaborn/pyDS_ch8_2_categorical_plots.py
+++ py4DS_ch_8_VIZ_p3_Seaborn/pyDS_ch8_2_categorical_plots.py
@@ -1,8 +1,23 @@
 
-################# 348: FULL
+################# 9.3
 # copy:  
 #        
 #        
-################# (03-Nov-24 for 04-Nov-24)
+################# (17-Nov-24 for 19-Nov-24)
 
 # Courses: PrTla PY for DS & ML >    9.3
+
+
+# ------------    categorical plots    ------------
+
+# Seaborn provides several plot types for visualizing categorical data, including:
+    # factorplot
+    # boxplot
+    # violinplot
+    # stripplot
+    # swarmplot
+    # barplot
+    # countplot
+
+
+
