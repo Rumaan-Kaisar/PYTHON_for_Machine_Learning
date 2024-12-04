51f1d6084a558eaecebf08c198defbb628e7ed2e
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-12-04 08:45:55 +0600

    Update pyDS_ch8_3_matrixPlot_grid.py

--- py4DS_ch_8_VIZ_p3_Seaborn/pyDS_ch8_3_matrixPlot_grid.py
+++ py4DS_ch_8_VIZ_p3_Seaborn/pyDS_ch8_3_matrixPlot_grid.py
@@ -1,5 +1,5 @@
 
-################# 9.4: 1.22
+################# 9.4: 2.38
 # copy:  
 #        
 #        
@@ -35,6 +35,19 @@ flights.head()
 # heatmap : primary way of showing the matrix plots
 # in order to work with heatmap, our data need to be in "matrix form"
 # "index name" and "column name" should match up so that 
-#   the "cell value" indicates something relavant to both of those index name and column name
+#       the "cell value" indicates something relavant to both of those index name and column name
 
+# matrix form
+# consider "tips" dataframe. Variables (columns and row index) needs to be same
+# we can use many methods
+    # 1. through a pivot table
+    # 2. Getting correlation data
+
+# using correlation
+tc = tips.corr()     # notice the same variable names in the column and row
+
+# creating heatmap
+sns.heatmap(tc)
+
+# rev[2.38 03-dec-2024]
 
