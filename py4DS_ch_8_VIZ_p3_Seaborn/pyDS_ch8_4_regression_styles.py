dd3de64f4445f11419b4c07ffec3f1baeb772f7d
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-12-17 13:46:57 +0600

    Update pyDS_ch8_4_regression_styles.py

--- py4DS_ch_8_VIZ_p3_Seaborn/pyDS_ch8_4_regression_styles.py
+++ py4DS_ch_8_VIZ_p3_Seaborn/pyDS_ch8_4_regression_styles.py
@@ -1,8 +1,34 @@
 
-################# 348: FULL
+################# 9.6
 # copy:  
 #        
 #        
-################# (03-Nov-24 for 04-Nov-24)
+################# (15-Dec-24 for 17-Dec-24)
 
 # Courses: PrTla PY for DS & ML >    9.6, 9.7
+
+
+# ------------    Regression Plots    ------------
+# Seaborn includes many tools for regression plots (Regression will be discussed later in the machine learning section)
+# For now, we'll focus on the lmplot() function
+
+# import libraries
+import seaborn as sns
+# shows figures in ipynb
+%matplotlib inline
+
+# To remove 'warnings' use 'filterwarnings'
+import warnings
+warnings.filterwarnings('ignore')
+
+# lets load a builtin dataset of seaborns: 'tips'
+tips = sns.load_dataset('tips')
+tips.head()
+
+
+# ----  lmplot  ----
+# lmplot: Displays linear models and allows:
+# Splitting plots by features.
+# Coloring using the hue parameter (based off of features)
+
+
