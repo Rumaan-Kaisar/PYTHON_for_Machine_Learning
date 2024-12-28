831732d1a84a8dbbf9c7560a6245bbe81da6c910
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-12-28 14:33:42 +0600

    Update pyDS_ch8_5_Exercise.py

--- py4DS_ch_8_VIZ_p3_Seaborn/pyDS_ch8_5_Exercise.py
+++ py4DS_ch_8_VIZ_p3_Seaborn/pyDS_ch8_5_Exercise.py
@@ -1,8 +1,43 @@
 
-################# 348: FULL
+################# 9.8
 # copy:  
 #        
 #        
-################# (03-Nov-24 for 04-Nov-24)
+################# (27-Dec-24 for 28-Dec-24)
 
 # Courses: PrTla PY for DS & ML >    9.8
+
+# ------------    seaborn exercise    ------------
+
+# Recreate the plots shown below (no need to match the exact colors, just focus on the plot itself)
+
+# Dataset
+    # We'll use the famous "Titanic" dataset for these exercises. 
+    # Later, in the Machine Learning section, we'll revisit this dataset to predict "passenger survival rates"
+    # For now, let's focus on visualizing the data with Seaborn.
+
+
+# import libraries
+import seaborn as sns
+import matplotlib.pyplot as plt
+# shows figures in ipynb
+%matplotlib inline
+
+# To remove 'warnings' use 'filterwarnings'
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# --------    "Titanic" dataset    --------
+# Using the Titanic dataset, recreate the plots below. 
+    # There are very few hints since most can be done with just one or two lines of code, 
+    # a hint would essentially reveal the solution
+    # so pay close attention to the "x and y labels" for hints.
+
+# lets load a builtin dataset of seaborns: 'titanic'
+ttnc = sns.load_dataset('titanic')
+ttnc.head()
+
+# plot age-fare distribution in jointpolt
+sns.jointplot(x='fare', y='age', data=ttnc, kind='scatter')
+
