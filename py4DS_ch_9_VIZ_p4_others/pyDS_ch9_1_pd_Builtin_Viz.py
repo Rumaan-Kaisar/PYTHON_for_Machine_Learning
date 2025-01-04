ee0236c407fea17f95d51603bcd14a005d1e0e02
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-01-04 13:25:56 +0600

    Update pyDS_ch9_1_pd_Builtin_Viz.py

--- py4DS_ch_9_VIZ_p4_others/pyDS_ch9_1_pd_Builtin_Viz.py
+++ py4DS_ch_9_VIZ_p4_others/pyDS_ch9_1_pd_Builtin_Viz.py
@@ -1,11 +1,26 @@
 
 ################# 10.1:
-# copy:
+# copy: csv files, py, ipynb
 #        
 #        
 ################# (03-Jan-25 for 04-Jan-25)
 
 # Courses: PrTla PY for DS & ML >    10.1, 10.2, 10.3
+
+
+# ------------    VISUALIZATION: Pandas Built-in Data Visualization    ------------
+# Pandas' built-in visualization capabilities are built on top of Matplotlib.  
+# These allow us to call visualization functions directly on a DataFrame.
+
+# import libraries
+import numpy as np
+import pandas as pd
+# shows figures in ipynb
+%matplotlib inline
+
+# To remove 'warnings' use 'filterwarnings'
+import warnings
+warnings.filterwarnings('ignore')
 
 
 
