2335fe82c5f78160f5c5a92cc63b660397405e02
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-01-05 14:09:24 +0600

    Update pyDS_ch9_1_pd_Builtin_Viz.py

--- py4DS_ch_9_VIZ_p4_others/pyDS_ch9_1_pd_Builtin_Viz.py
+++ py4DS_ch_9_VIZ_p4_others/pyDS_ch9_1_pd_Builtin_Viz.py
@@ -1,9 +1,9 @@
 
-################# 10.1:
+################# 10.1: 2.55
 # copy: csv files, py, ipynb
 #        
 #        
-################# (03-Jan-25 for 04-Jan-25)
+################# (04-Jan-25 for 05-Jan-25)
 
 # Courses: PrTla PY for DS & ML >    10.1, 10.2, 10.3
 
@@ -15,12 +15,42 @@
 # import libraries
 import numpy as np
 import pandas as pd
+import seaborn as sns
+
 # shows figures in ipynb
 %matplotlib inline
 
 # To remove 'warnings' use 'filterwarnings'
 import warnings
 warnings.filterwarnings('ignore')
+
+
+# --------    importing dataset    --------
+#  Notice we've specified the index
+df1 = pd.read_csv('./y_dataFrame_1.csv', index_col=0)
+# notice the index is actually dates (i.e. a time-series)
+df1.head()
+
+# following dataset is non-time-series
+df2 = pd.read_csv('./y_dataFrame_2.csv', index_col=0)
+# notice, it has sequential index insted of time-series
+df2.head()
+
+
+# --------  What can we actually do with these types of DataFrames?  --------
+
+# histogram of 'A' column from df1
+df1['A'].hist()     # notice it actually calling "matplotlib"
+
+# so we can add "matplotlib" or "sns" style arguments
+df1['A'].hist(bins=30)     # notice it actually calling "matplotlib"
+
+# styling with "seaborn": no effect
+    # just import the package, "matplotlib" autometically apply the style
+df1['A'].hist(bins=30, edgecolor='black', linewidth=0.5)
+
+
+# --------    pandas built-in plot types    --------
 
 
 
