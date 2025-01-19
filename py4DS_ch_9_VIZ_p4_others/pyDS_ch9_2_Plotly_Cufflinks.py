ce2a03dfb6393f67422243164888cc9d92b2d730
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-01-19 11:58:52 +0600

    Update pyDS_ch9_2_Plotly_Cufflinks.py

--- py4DS_ch_9_VIZ_p4_others/pyDS_ch9_2_Plotly_Cufflinks.py
+++ py4DS_ch_9_VIZ_p4_others/pyDS_ch9_2_Plotly_Cufflinks.py
@@ -1,5 +1,5 @@
 
-################# 11.1:
+################# 11.1: 0.44
 # copy:
 #        
 #        
@@ -7,5 +7,37 @@
 
 # Courses: PrTla PY for DS & ML >    11.1, 11.2
 
+# ------------    Plotly and cufflinks    ------------
+
+# NXT >> [GPT edit]
+
+# WE'LL LKEARN the bit of the background behaing of the plotly and cuflinks
+# which aloows us to make interactive visualizatiobnsd
 
 
+# "Plotly" is an interactive visualization library (opensource) and "Cufflinks" connects plotly with pandas
+# Before we explore these libraries you'll need to install them
+
+# therefore we can make interactive visualization directly off of our datafarme
+
+# before explore these libraries we need to install them
+# You’ll need to install it with pip at your command line or terminal with:
+# pip install plotly and
+# pip install cufflinks
+
+
+# offical website: https://plotly.com/ (old: plot.ly)
+# plotly: it's also a hoting company that can host your visualization and dashbords
+# if you do it yourself, the library is free and opensource
+# plotly is compatable to: python, R, MatLab, Excel, jaVASCRIPT
+# WE'LL FOCUS IT USING IN PYTHON
+
+# getting started with python: https://plotly.com/python/
+# Is Plotly for Python Free?
+# Plotly for Python is free and open-source software, licensed under the MIT license. It costs nothing to install and use.
+# You can use Plotly for Python to make, view, and distribute charts and maps without registering for any service, obtaining any token, or creating any account. 
+# You can use Plotly for Python to make, view, and distribute graphics totally offline.
+
+
+# cufflinks
+# https://pypi.org/project/cufflinks/
