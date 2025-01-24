1c029d34d0929a6821f8a468ed3fa436c5993ae2
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-01-24 12:55:08 +0600

    Update pyDS_ch9_2_Plotly_Cufflinks.py

--- py4DS_ch_9_VIZ_p4_others/pyDS_ch9_2_Plotly_Cufflinks.py
+++ py4DS_ch_9_VIZ_p4_others/pyDS_ch9_2_Plotly_Cufflinks.py
@@ -1,9 +1,9 @@
 
 ################# 11.1: full, 11.2:
-# copy:
+# copy: plotly.js, local_plot.html, embeded_html
 #        
 #        
-################# (21-Jan-25 for 22-Jan-25)
+################# (22-Jan-25 for 24-Jan-25)
 
 # Courses: PrTla PY for DS & ML >    11.1, 11.2
 
@@ -48,13 +48,18 @@
 # or    pip install plotly==5.24.1
 # or    conda install -c plotly plotly=5.24.1
 
-# test plotly:
+
+# --------    Test Plotly    --------
+
+# test plotly: Makes an "HTML" file
+    # The HTML file will embed Plotly.js (so the HTML will be havier, > 4.5mb)
+    # However, we can use 'cdn' to imbed Plotly.js, makes the HTML much light-weight
 import plotly.express as px
 fig = px.bar(x=["a", "b", "c"], y=[1, 3, 2])
 fig.write_html('first_figure.html', auto_open=True)
 
 
-# Standalone HTML:
+# plotly.offline, Standalone HTML:
     # Offline mode will save an HTML file locally and open it inside your web browser.
 
 import plotly
@@ -83,6 +88,7 @@ plotly.offline.plot({
 
 
 
+
 # ------------  import libraries, setup  ------------
 # import libraries
 import numpy as np
@@ -101,4 +107,60 @@ warnings.filterwarnings('ignore')
 import plotly
 print(plotly.__version__)   # version > 1.9.4 required
 
+
+
+
+# --------    Show pltly figures in jypyter nb    --------
+
+# Use plotly in ipynb: render through Plotly.js but not saved to notebook (not recomended)
+    # ERR: Mime type rendering requires nbformat>=4.2.0 but it is not installed
+    # pip install --upgrade nbformat
+import plotly.express as px
+fig = px.bar(x=["a", "b", "c"], y=[1, 3, 2])
+fig.show()
+
+
+# another way to see it in notebook (not recomended)
+# use offline version
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+# For Notebooks
+init_notebook_mode(connected=True)
+# For offline use
+cf.go_offline()
+
+# create a DataFrame
+df = pd.DataFrame(np.random.randn(100,4),columns='A B C D'.split())
+df.head()
+
+# show plotly in notebook
+df.iplot(kind='scatter',x='A',y='B',mode='markers',size=10)
+
+
+
+
+# --------    use 'cdn' or 'plotly-2.35.2.min.js in working directory'    --------
+    # The HTML file's large size is primarily due to the embedded Plotly.js.
+    # To reduce the size, we can use 'cdn' to load Plotly.js externally.
+    # Alternatively, we can download "Plotly.js" to our working directory and reference it locally.
+
+# using 'cdn'
+import plotly.io as pio
+import plotly.express as px
+
+fig = px.bar(x=["a", "b", "c"], y=[1, 3, 2])
+pio.write_html(fig, 'plot_lc.html', include_plotlyjs='cdn')
+
+
+# ---- Best way: use 'cdn' or local "plotly.js" file path ----
+# For a minimal offline approach, follow these steps:
+#   1. Download the Plotly.js file (e.g., plotly-2.35.2.min.js).
+#   2. Create the HTML using plotly.io and set include_plotlyjs='.\plotly-2.35.2.min.js'.
+#   3. This uses the local "plotly.js" file path instead of 'cdn'.
+
+# using local "Plotly.js"
+import plotly.io as pio
+import plotly.express as px
+
+fig = px.bar(x=["a", "b", "c"], y=[1, 3, 2])
+pio.write_html(fig, 'plot_lc.html', include_plotlyjs='.\plotly-2.35.2.min.js')
 
