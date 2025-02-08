7855a17eeb3ac0590b3f1e47572d381e71f7521d
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-02-08 16:39:39 +0600

    Update pyDS_ch9_3_GeoPlot_Choropleth.py

--- py4DS_ch_9_VIZ_p4_others/pyDS_ch9_3_GeoPlot_Choropleth.py
+++ py4DS_ch_9_VIZ_p4_others/pyDS_ch9_3_GeoPlot_Choropleth.py
@@ -1,11 +1,28 @@
 
-################# 12.1:
+################# 12.1: full, 12.2
 # copy:
 #        
 #        
 ################# (03-Jan-25 for 04-Jan-25)
 
 # Courses: PrTla PY for DS & ML >    12.1, 12.2, 12.3, 12.4, 12.5
+
+
+# ----------------    Geographical Plotting    ----------------
+# more: https://plotly.com/python/choropleth-maps/
+
+# Choropleth Maps:
+    # A Choropleth map uses colors to represent data across geographic regions.  
+
+# Challenges in Geographical Plotting:  
+    # Data often comes in varying formats (e.g., GeoJSON, Shapefiles).  
+    # Requires specialized libraries for specific datasets.  
+
+# Why Plotly?  
+    # Built-in interactive features for zooming, hovering, and customization.  
+    # Matplotlib's "Basemap" is deprecated (use "Cartopy" instead), but Plotly is simpler for web-ready maps.  
+
+# Let's create a Choropleth map using Plotly!  
 
 
 
