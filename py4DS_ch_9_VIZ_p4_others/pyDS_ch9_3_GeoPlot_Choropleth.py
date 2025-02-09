b2696cbcb66c993549b1cb207465e61c138a4681
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-02-09 14:34:57 +0600

    Update pyDS_ch9_3_GeoPlot_Choropleth.py

--- py4DS_ch_9_VIZ_p4_others/pyDS_ch9_3_GeoPlot_Choropleth.py
+++ py4DS_ch_9_VIZ_p4_others/pyDS_ch9_3_GeoPlot_Choropleth.py
@@ -1,9 +1,9 @@
 
-################# 12.1: full, 12.2
+################# 12.1: full, 12.2: 2.41
 # copy:
 #        
 #        
-################# (03-Jan-25 for 04-Jan-25)
+################# (08-Feb-25 for 09-Feb-25)
 
 # Courses: PrTla PY for DS & ML >    12.1, 12.2, 12.3, 12.4, 12.5
 
@@ -22,7 +22,40 @@
     # Built-in interactive features for zooming, hovering, and customization.  
     # Matplotlib's "Basemap" is deprecated (use "Cartopy" instead), but Plotly is simpler for web-ready maps.  
 
-# Let's create a Choropleth map using Plotly!  
 
+
+# Let's create a Choropleth map using Plotly!  
+# It allows us to plot information on either a global or nationwide scale.  
+# Choropleth maps can be challenging at first due to Plotly's syntax,  
+    # so we'll use this notebook for reference.
+    # also ther's a "cheat sheet" pdf for plotly:
+        # https://images.plot.ly/plotly-documentation/images/python_cheat_sheet.pdf
+
+# We'll focus on:  
+    # Nationwide scale plots  
+    # Real-world data with larger nationwide plots  
+    # World-scale plots
+
+# import libraries
+import numpy as np
+import pandas as pd
+import cufflinks as cf
+
+# shows figures in ipynb
+%matplotlib inline
+
+# To remove 'warnings' use 'filterwarnings'
+import warnings
+warnings.filterwarnings('ignore')
+
+# Enable offline mode for interactive plotting
+cf.go_offline()
+
+
+# using local "Plotly.js"
+import plotly.io as pio
+import plotly.plotly as ptly
+import plotly.express as px
+from plotly.offline import iplot
 
 
