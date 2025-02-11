00c7ffe61eda704a881bba226ccd48770ddb9ed8
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-02-11 14:00:17 +0600

    Update pyDS_ch9_3_GeoPlot_Choropleth.py

--- py4DS_ch_9_VIZ_p4_others/pyDS_ch9_3_GeoPlot_Choropleth.py
+++ py4DS_ch_9_VIZ_p4_others/pyDS_ch9_3_GeoPlot_Choropleth.py
@@ -1,9 +1,9 @@
 
-################# 12.1: full, 12.2: 2.41
+################# 12.1: full, 12.2: 5.40
 # copy:
 #        
 #        
-################# (08-Feb-25 for 09-Feb-25)
+################# (09-Feb-25 for 11-Feb-25)
 
 # Courses: PrTla PY for DS & ML >    12.1, 12.2, 12.3, 12.4, 12.5
 
@@ -57,5 +57,33 @@ import plotly.io as pio
 import plotly.plotly as ptly
 import plotly.express as px
 from plotly.offline import iplot
+
+
+# first we build our data-dictionary
+# We'll use the dict() method to create a dictionary.
+    # to cast a list to the dictionary
+
+data = dict(type = 'choropleth',
+            locations = ['AZ', 'CA', 'NY'],
+            loactionmode = 'USA-states',
+            colorscale = 'Portland',
+            text = ['text 1', 'text 2', 'text 3'],
+            z = [1.0, 2.0, 3.0],
+            colorbar = {'title':'colorbar title goes here'}
+        )
+
+# Define key elements:
+    # "type": Set as 'choropleth'
+    # "locations": List of state abbreviations (e.g., ['AZ', 'CA', 'NY']).
+    # "locationmode": Set to 'USA-states'. It'll let plotly know that we're doing this in US-level
+        # theres' also different location modes, more in "documentation"
+    # "colorscale": Choose a predefined color scheme (e.g., 'Portland') 
+        # we can however use other colorscaleas such as green, grays
+    # "text": List of hover text values, i.e. each of the locations.
+    # "z": actual values shown in to the coloscale.
+    # "colorbar": Dictionary defining the "color bar title".
+
+# We use this kind of notation because it follows Plotly's documentation.
+
 
 
