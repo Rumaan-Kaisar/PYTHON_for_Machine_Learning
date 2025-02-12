12a4383aea5c643722eb3e166da1d9b905763ee5
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-02-12 13:27:51 +0600

    Update pyDS_ch9_3_GeoPlot_Choropleth.py

--- py4DS_ch_9_VIZ_p4_others/pyDS_ch9_3_GeoPlot_Choropleth.py
+++ py4DS_ch_9_VIZ_p4_others/pyDS_ch9_3_GeoPlot_Choropleth.py
@@ -3,7 +3,7 @@
 # copy:
 #        
 #        
-################# (09-Feb-25 for 11-Feb-25)
+################# (11-Feb-25 for 12-Feb-25)
 
 # Courses: PrTla PY for DS & ML >    12.1, 12.2, 12.3, 12.4, 12.5
 
@@ -54,10 +54,44 @@ cf.go_offline()
 
 # using local "Plotly.js"
 import plotly.io as pio
-import plotly.plotly as ptly
+# import plotly.plotly as ptly      # deprected
 import plotly.express as px
 from plotly.offline import iplot
 
+
+
+
+""" 
+        Now, we need to begin building our data dictionary. 
+        The easiest way to do this is by using the dict() function with the following general structure:  
+
+        type = 'choropleth'
+        locations = List of states  
+        locationmode = 'USA-states'
+        colorscale = Choose a predefined or custom scale  
+        text = List or array of text to display per point  
+        z = Array of values for the z-axis (determining state color)
+        colorbar = {'title': 'Colorbar Title'}
+"""
+# Choosing a Colorscale
+# You can select from predefined color scales, such as:  
+    # 'pairs'
+    # 'Greys'
+    # 'Greens'
+    # 'Bluered'
+    # 'Hot'
+    # 'Picnic'
+    # 'Portland'
+    # 'Jet'
+    # 'RdBu'
+    # 'Blackbody'
+    # 'Earth'
+    # 'Electric'
+    # 'YIOrRd'
+    # 'YIGnBu'
+
+# or create a custom colorscale (https://plot.ly/python/heatmap-and-contour-colorscales/)
+# Here we make it like below:
 
 # first we build our data-dictionary
 # We'll use the dict() method to create a dictionary.
@@ -65,7 +99,7 @@ from plotly.offline import iplot
 
 data = dict(type = 'choropleth',
             locations = ['AZ', 'CA', 'NY'],
-            loactionmode = 'USA-states',
+            locationmode = 'USA-states',
             colorscale = 'Portland',
             text = ['text 1', 'text 2', 'text 3'],
             z = [1.0, 2.0, 3.0],
@@ -86,4 +120,40 @@ data = dict(type = 'choropleth',
 # We use this kind of notation because it follows Plotly's documentation.
 
 
+# Now we create a Layout object/variable, it'll be a nested dictionary
+layout = dict(geo={'scope':'usa'})
 
+# Notice that we have two main objects:
+    # 1. 'data' in dictionary form
+    # 2. 'layout'
+
+# These are then passed into graph_objs.Figure()
+# Finally, the figure is plotted using iplot
+
+# so we need to use graph_objs.figure
+import plotly.graph_objs as go
+
+choromap_1 = go.Figure(data= [data], layout=layout)
+# notice, "data" is already in dict form, but we're putting it inside a list
+
+# ----  Plot the figure  ----
+# fig1 = iplot(choromap_1, asFigure=True)
+# notice that, here "iplot" is from "plotly.offline" not "cufflinks "
+    # here, it just displays the plot
+    # so "asFigure=True" won't work here
+# since it's not a dict or Figure, 
+    # we must use "choromap_1" directly from go.Figure() to save as HTML
+
+# ----  Save the figure as an HTML file  ----
+# pio.write_html(fig1, 'choromap_1.html', include_plotlyjs='./plotly-2.35.2.min.js')    # ERR
+
+# The reason why df1.iplot(asFigure=True, kind='scattermatrix') works while 
+    # iplot(choromap_1, asFigure=True) does not is because they come from different libraries.
+        # df1.iplot() is from cufflinks
+        # iplot(choromap_1) is from plotly.offline
+
+# display the plot
+iplot(choromap_1)
+
+# Save the figure as an HTML file
+pio.write_html(choromap_1, 'choromap_1.html', include_plotlyjs='./plotly-2.35.2.min.js')
