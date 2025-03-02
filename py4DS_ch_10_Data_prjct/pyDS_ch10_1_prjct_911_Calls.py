de588e3270aab2a026c841620900141636094c98
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-03-02 16:06:42 +0600

    Add files via upload

---
+++ py4DS_ch_10_Data_prjct/pyDS_ch10_1_prjct_911_Calls.py
@@ -0,0 +1,10 @@
+
+################# 10.1:
+# copy: topics txt, py(s)
+#        
+#        
+################# (01-Mar-25 for 02-Mar-25)
+
+# Courses: PrTla PY for DS & ML >    10.1, 10.2, 10.3, 10.4
+
+
---
+++ py4DS_ch_10_Data_prjct/pyDS_ch10_2_prjct_Finance.py
@@ -0,0 +1,10 @@
+
+################# 10.6:
+# copy: topics txt, py(s)
+#        
+#        
+################# (01-Mar-25 for 02-Mar-25)
+
+# Courses: PrTla PY for DS & ML >    10.6, 10.7, 10.8, 10.9
+
+
