debda9607d2aa620946d40ed374845c20b6d770a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-03-04 17:18:49 +0600

    Update pyDS_ch10_1_prjct_911_Calls.py

--- py4DS_ch_10_Data_prjct/pyDS_ch10_1_prjct_911_Calls.py
+++ py4DS_ch_10_Data_prjct/pyDS_ch10_1_prjct_911_Calls.py
@@ -1,10 +1,28 @@
 
 ################# 10.1:
-# copy: topics txt, py(s)
+# copy: data, py, ipynb
 #        
 #        
-################# (01-Mar-25 for 02-Mar-25)
+################# (02-Mar-25 for 04-Mar-25)
 
 # Courses: PrTla PY for DS & ML >    10.1, 10.2, 10.3, 10.4
+
+
+# ----------------    project: Emergency - 911 Calls    ----------------
+
+# For this project, we'll analyze 911 call data from Kaggle (https://www.kaggle.com/mchirico/montcoalert)
+# Since the dataset is large, we'll use a smaller version from any GitHub repo with similar data. 
+
+# --------  About dataset:  --------
+    # The dataset includes the following fields:
+        # lat : String variable, Latitude
+        # lng: String variable, Longitude
+        # desc: String variable, Description of the Emergency Call
+        # zip: String variable, Zipcode
+        # title: String variable, Title
+        # timeStamp: String variable, YYYY-MM-DD HH:MM:SS
+        # twp: String variable, Township
+        # addr: String variable, Address
+        # e: String variable, Dummy variable (always 1)
 
 
