1273faf59358ea6ff3ae8bc5f5a6df3c7e375c9c
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-03-05 16:54:32 +0600

    Update pyDS_ch10_1_prjct_911_Calls.py

--- py4DS_ch_10_Data_prjct/pyDS_ch10_1_prjct_911_Calls.py
+++ py4DS_ch_10_Data_prjct/pyDS_ch10_1_prjct_911_Calls.py
@@ -1,9 +1,9 @@
 
 ################# 10.1:
-# copy: data, py, ipynb
+# copy: py, ipynb
 #        
 #        
-################# (02-Mar-25 for 04-Mar-25)
+################# (04-Mar-25 for 05-Mar-25)
 
 # Courses: PrTla PY for DS & ML >    10.1, 10.2, 10.3, 10.4
 
@@ -26,3 +26,31 @@
         # e: String variable, Dummy variable (always 1)
 
 
+# ----  Getting bacsic info from the dataset  ----
+
+# import libraries
+import numpy as np
+import pandas as pd
+
+# We'll use following visualization libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# shows figures in ipynb
+%matplotlib inline
+
+# To remove 'warnings' use 'filterwarnings'
+import warnings
+warnings.filterwarnings('ignore')
+
+# Read in the csv file and as a dataframe
+df = pd.read_csv("./data_call_911_small.csv")
+
+# data info
+df.info()
+
+# inspect first 5 row
+df.head()
+
+
+# NXT >> Basic Q in[134]
