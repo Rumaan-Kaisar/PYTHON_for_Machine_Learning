5fa6e124c9f7b161ca7b91214c6cf2ee03a69bb2
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-05-04 20:15:55 +0600

    Update pyDS_ch11_2_performnace.py

--- py4DS_ch_11_ML_intro/pyDS_ch11_2_performnace.py
+++ py4DS_ch_11_ML_intro/pyDS_ch11_2_performnace.py
@@ -7,8 +7,16 @@
 
 # Courses: PrTla PY for DS & ML >    14.4, 14.5
 
+
+
+
 """ 
-Analysis of the Text: Performance Evaluation for Classification Models
+
+
+
+Performance Evaluation for Classification Models:
+
+
 1. Core Topics Identified:
 Introduction to Model Evaluation
 
@@ -236,3 +244,214 @@ Need practical examples? Ask about applying these metrics to specific datasets!
 
  """
 
+
+
+
+
+
+
+
+
+
+
+"""  
+----------------------    Use google translate:    -----------------------------
+
+Welcome everyone to this lecture on performance evaluation, particularly for classification problems, in the
+
+next lecture.
+
+We'll be discussing performance evaluation for regression protests. So, we just learned that after
+
+a machine learning process is complete, we'll use performance metrics to evaluate how our model actually performed.
+
+And we keep mentioning the fact that we train our model on the training data and then we'll use
+
+some kind of metrics to actually see how it performed on the test set or the validation set.
+
+So let's go ahead and discuss what that really means.
+
+What classification metrics are we going to use and the key classification metrics?
+
+We should understand our accuracy, precision, recall, and f1 score. 
+
+
+
+But first, we need to understand the reasoning behind these metrics and how they will actually perform in the real world. 
+Typically, in any classification task, your model can actually only achieve two outcomes: either your model was correct in its prediction, or your model was incorrect in its prediction. 
+And all classification metrics stem from this idea.
+
+--------------------------  1.12
+
+ Now,
+
+Fortunately, incorrect versus correct also expands to situations where you have multiple classes, such as trying to predict categories of more than two.
+
+For example, you have Category A, B, C, D. You can either be correct in predicting the correct category or incorrect in predicting the correct category.
+
+Now, for the purpose of explaining the metrics, let's go ahead and simplify this to what is known as a binary classification situation, which means two.
+
+So, there are only two classes available: either class 0 or class 1.
+
+And this idea will also expand to multiple classes.
+
+But for simplicity, let's just imagine a binary classification situation, so in our example, we'll try to predict whether an image
+
+is a dog or a cat. The neural network part of this course will actually perform this task later during the convolution of the entire
+
+course.
+
+Now, since this is a supervised learning problem, what we'll need to do first is fit or train a model
+
+on training data.
+
+That means we'll have images that someone has already guessed and labeled as either a dog or a cat.
+
+So we know the correct answer in these images, and then we're going to test that model on the test data.
+
+So we're going to show it new images that the model hasn't seen before.
+
+Get the model's prediction and then compare the model's prediction results with the
+
+correct answer that we already know.
+
+So, once we have the model's predictions from the test data x, we compare it to the true y values.
+
+The correct labels, so we actually diagram this process.
+
+Let's imagine we've already trained our model on some training data, and now it's time to actually evaluate
+
+the model's performance.
+
+This is where our test dataset comes in.
+
+So we take a test image, which we're going to label "x" (test X), which stands for "feature."
+
+So the image itself is a feature, and this is from the test set, and there's a corresponding correct label from test Y.
+
+So we have feature or image X, and that's the test image, and we also have the correct label from test Y.
+
+And in this case, we can see that this image is an image of a dog.
+
+So, what we're going to do is feed the features.
+
+In this case, we'll feed the image into our already trained model, and then the model will make some predictions.
+
+So, the model predicts that it's a dog, and then what we do is compare the prediction with the correct label.
+
+So, this dog, equal to dog, in this case was correct; however, it might have predicted this image to be a cat.
+
+And in this case, this comparison with the correct label would be incorrect.
+
+And these are essentially the only two situations.
+
+You're either right or wrong, correct or incorrect, so we repeat this process for all the images in our test data x.
+
+And in the end, we'll have a count of correct matches and a count of incorrect matches.
+
+And the key realization we need to make here is that in the real world, not all incorrect or correct matches have the same value.
+
+So, let's focus on what we mean by that, so that in the real world, a single metric won't tell the whole story. To understand all of this, let's go back to those four metrics we mentioned and see how they're actually calculated.
+
+We could organize our predicted values ​​against the actual values ​​in what is known as a confusion matrix, so
+
+
+
+
+
+
+
+
+
+Part 2: 
+
+Welcome back, everyone.
+
+We just discussed how to evaluate performance for classification problems.
+
+Now let's understand how to evaluate performance for regression testing, so we can take a moment to discuss how we evaluate a model that is performing some type of regression task.
+
+And, in general, a regression is the task when a model tries to predict continuous values.
+
+Unlike classification tasks, where we are trying to predict categorical values, you may have heard of some evaluation metrics, such as precision or recall, as we just discussed.
+
+However, these types of metrics will not be useful for regression problems.
+
+It doesn't really make sense to calculate the precision or recall of a regression task since you are actually classifying things.
+
+Instead, you are predicting a continuous value.
+
+Therefore, we need new metrics designed for those continuous values.
+
+For example, imagine we are trying to predict the price of a house given its characteristics.
+
+That would be a regression task, or trying to predict the country.
+
+A house is inside.
+
+Given its characteristics, it would be a classification task.
+
+And again, we're focused now on how to evaluate that regression task where a label is continuous and not separate categories.
+
+So, again, to look at the most common evaluation metrics for regression, which are the
+
+mean absolute error (mean squared error) and then the root mean squared error.
+
+Let's start with the simplest, which is the mean absolute error.
+
+And this is the easiest one to understand. Essentially, all you're going to do is compare your predictions.
+
+And remember, we're comparing a continuous value here since this is a regression task.
+
+We're going to compare our predictions with the label and the true one. For example, compare the actual house price prediction with the actual house price.
+
+And what we do is simply take the difference between the true price minus our predicted price. That's why we take the absolute value of that. The reason we take the absolute value is because technically, our predictions could be above or below the true value.
+
+And since we want to average all of our predictions, we take the absolute value.
+
+Okay, that's the mean absolute error. You're essentially just taking the differences between your prediction and the true label. You take the absolute value of that, and then you average that for all of your predictions.
+
+Now, the problem with the mean absolute error is that it won't punish large errors.
+
+So here we have what's known as an ans comes quartet, and we can see here that we have a wide variety
+
+of different spreads of data points here.
+
+However, the line of best fit for all of these is actually the same.
+
+So we want to make sure we're aware of situations like this.
+
+For example, let's take a look at this specific situation.
+
+We have one point that's a huge outlier, so we want our air metrics to really account for them.
+
+So what we can do is use a mean squared error.
+
+And this is the mean of the squared errors.
+
+So what we're doing here is taking the difference between the true value minus our predicted value and we're going to square it.
+
+And as you can imagine, when we actually square that error, the larger errors are more noticeable than with mean absolute error, which makes squared error more popular because it's actually going to punish your model for those outliers it doesn't fit.
+
+And we no longer need to take the absolute value because anything squared ends up being positive.
+
+However, there's another problem we run into with a squared error that squares the actual label minus its prediction.
+
+It actually also squares the units themselves.
+
+So, for example, if you're predicting the price of a house, our error metric with mean absolute error would be in dollars, but with mean squared error.
+
+We end up getting an error metric in units of dollars squared, which is hard to interpret, so the way we solve this is with the root mean squared error. Essentially, at the end, you just take the square root of your mean squared error.
+
+So this is the most popular because it punishes those larger error values ​​and
+
+at the end of the day, it has the same metrics or the same units as y now.
+
+The most common question I get from students is: Hey, does this squared error mean this value?
+
+I got it right.
+
+And as always, context is everything.
+
+Let's imagine you were doing some kind of machinery model and got a squared error.
+"""
