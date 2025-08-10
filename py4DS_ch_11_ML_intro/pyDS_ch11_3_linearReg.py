743a7a51ffc29490fc2cf87f40b728f562aadf3a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-08-10 13:55:12 +0600

    Update pyDS_ch11_3_linearReg.py

--- py4DS_ch_11_ML_intro/pyDS_ch11_3_linearReg.py
+++ py4DS_ch_11_ML_intro/pyDS_ch11_3_linearReg.py
@@ -14,34 +14,139 @@
 
 """ 
     Project 1:
-        Now we'll use scikit-learn in Python to create a linear regression model.
-
-        After that, we'll work on your own portfolio project exercise, and once completed, the solutions will be reviewed and discussed.
+        Now we'll use scikit-learn in Python to create a linear regression model using "Housing price data".
 
 """
 
 
-Linear Regression with Python: Practical Implementation
+# Example 1: Predicting house price
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+
+# read data
+df = pd.read_csv('data_USAhousing.csv')
+df.head()
+
+
+# ----  check info  ----
+df.info()
+df.describe()
+df.columns
+
+
+# ----  basic visulaization and analysis  ----
+# sns.pairplot(df, height=2, aspect=1.3)
+
+# save as image, then load it in "markdown cell"
+plot = sns.pairplot(df, height=2, aspect=1.3)
+plot.savefig("./ch11_img/house_pairplot.png", dpi=50)
+plt.close() # prevents the plot from displaying and embedding
+
+
+# ----  histogram  ----
+sns.histplot(df['Price'], kde=True, shrink=0.95, edgecolor='white', color="#23aae3")
+
+# Modern font settings
+plt.rcParams["font.family"] = "DejaVu Sans"
+
+# Title and axis labels
+plt.title("House price distribution in $M", fontsize=15, color="#54626f", pad=15)
+plt.xlabel("Price $M", fontsize=12, color="#87a96b")
+plt.ylabel("Count", fontsize=12, color="#87a96b")
+
+# Ticks and grid
+plt.xticks(fontsize=10, color="#54626f")
+plt.yticks(fontsize=10, color="#54626f")
+plt.grid(axis="y", linestyle="--", linewidth=0.5, alpha=0.6, color="#72a0c1")
+
+# Minimalist styling
+for spine in plt.gca().spines.values():
+    spine.set_visible(False)
+
+
+# ----  Heatmap  ----
+# numeric_only=True to avoid strings
+dt_crr = df.corr(numeric_only=True)
+dt_crr
+
+sns.heatmap(dt_crr, cmap='rocket')
+sns.heatmap(dt_crr, cmap='rocket', annot=True)
+
+
+# ----------------    Model creation    ----------------
+df.columns
+
+X = df[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms',
+       'Avg. Area Number of Bedrooms', 'Area Population']]
+y = df['Price']
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
+lm = LinearRegression()
+lm.fit(X_train, y_train)    # notice the output -> it has been trained
+
+
+# ----  evaluating model performance  ----
+print(lm.intercept_)
+lm.coef_
+X_train.columns
+X.columns
+
+# coefficent analysis
+import pandas as pd
+cdf = pd.DataFrame(lm.coef_, X.columns, columns = ['Coeff'])
+# lm.coef_ as data. 
+# X.columns as index
+# columns = ['Coeff'] names the column as 'Coeff'
+print(cdf)
+
+# ----  accuracy  ----
+predictions = lm.predict(X_test)
+print(predictions)     # these are the predicted prices
+print(y_test)  # these are the actual prices
+
+# Create the scatterplot
+plt.figure(figsize=(10, 6))
+plt.scatter(y_test, predictions, color="#89cff0", edgecolors='#318ce7', s=10, alpha=0.8)
+
+# histogram for "residuals"
+sns.histplot(y_test - predictions, kde=True, shrink=0.95, edgecolor='white', color="#1abcdf")
+
+
+# ----  Evaluation matrix  ----
+from sklearn import metrics
+import numpy as np
+
+# MAE
+metrics.mean_absolute_error(y_test, predictions)
+
+# MSE
+metrics.mean_squared_error(y_test, predictions)
+
+# RMSE
+np.sqrt(metrics.mean_squared_error(y_test, predictions))
+
 
 -------------------------------------
 
 # NXT >> add codes from ipynb here
 
 
----- Check Point 3 : clarify
-
- 
-Ok like I mentioned you can go ahead and explore the Boston data set that I mentioned earlier and that's
-linked in your note book.
-If you want to redo this analysis on a real dataset.
-But coming up next is going to be your exercise for your portfolio project.
-Or we're going to give you some e-commerce data and you'll have to create a linear regression model
-by following the directions in the exercise and then afterwards we'll go over the solutions.
-I hope you enjoy your first foray into machine learning.
-Later on we'll delve into real data sets from Kaggle dot com and perform machine learning projects that
-are even more in-depth.
-Thanks everyone and I'll see you at the next lecture
 
 
-# rev[08-Aug-2025]  copy all the code here from ipynb
+"""  
+    portfolio project exercise: Ecommerce analysis
+"""
+
+
+
+
 
