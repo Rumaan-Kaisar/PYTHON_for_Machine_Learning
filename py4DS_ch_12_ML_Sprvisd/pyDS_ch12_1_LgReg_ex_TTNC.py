3154686033b8ea78865412bed96e541d77574e8a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-05-24 14:03:47 +0600

    Update pyDS_ch12_1_LgReg_ex_TTNC.py

--- py4DS_ch_12_ML_Sprvisd/pyDS_ch12_1_LgReg_ex_TTNC.py
+++ py4DS_ch_12_ML_Sprvisd/pyDS_ch12_1_LgReg_ex_TTNC.py
@@ -10,3 +10,66 @@
 # polish in textbook form
 # compare and simplified pointwise
 
+
+""" 
+-------------  https://www.kaggle.com/
+
+Hello everyone and welcome to the logistic regression of Python lecture I'm really excited for this
+lecture because we're going to be working with a titanic data set.
+This is a very famous dataset and it's often a student's first step in machine learning FOR classification
+.
+We're going to be trying to predict the classification survival or the CCed for passengers who were
+on the Titanic and we're going to be using a semi clean version of this titanic data set in the dataset
+.
+Original source is actually from "Kaggle dot com".
+
+
+Let me go ahead and briefly explain what Kaggle dot com is.
+I'm going to go ahead and jump to that Web site in my browser.
+OK.
+So here I am on Kaggle or Kaggle dot com pending how you pronounce it but it's K a G G L E dot com.
+And basically this is a data science hosting Web site.
+It has two or three main aspects to it.
+
+Dataset:
+    The first aspect is when we're actually already familiar with which is the data sets aspect.
+Basically this is a Web site that can host data sets for instance if you're interested in one of these
+data sets you can kind of scroll down here.
+Check out some of these data set such as "airplane crashes since 1908".
+<link>
+You can go ahead and click on this link and load up the data set a way to download it and some information
+on that data set.
+For instance here you can go ahead and click valid data set and read a description of the data set.
+And sometimes even has open ended questions free to answer just as a curiosity.
+
+
+competition:
+    The other aspect of this Web site is the competition's page.
+    You go ahead and click on the competition page.
+    It'll take you to a list of all the active competitions and basically this is the place where companies
+    can post their own data sets with their own problems and ask either a single person or a team of people
+    to compete for a monetary prize in order to solve their own real company problems.
+    Sometimes they're offering money other times or offering jobs at their corporation.
+    So you'll see things such as Facebook posts a challenge on this Web site and if you do well in the challenge
+    they'll actually ask you to interview at their company and other challenges are just for knowledge.
+    So you can see these simpler challenges here actually just knowledge based challenges 
+
+    and the one we're
+    going to do is the Titanic machine learning from disaster challenge.
+    And this is the basic source of where we're actually going to be getting our data from.
+    <link>
+
+    Now this data is actually already been downloaded for you and it's part of the whole package you downloaded
+    files notebooks.
+    Let me go in and show you where to find it.
+    If you go ahead and jump to the machine learning sections folder under logistic regression you'll see
+    some files here.
+    And the Titanic to see V and Titanic trained csv files are the data sets we're going to be working
+    with.
+    So go ahead and use these two csv files that have already been downloaded for you and the notebook we're
+    going to be using is logistic regression of Python.
+    Later on we'll show you how to do the project and then we'll show the project solutions.
+    But right now let's go ahead and jump straight to a new notebook.
+    Or you can go ahead and follow along with the lecture notebook but I'm going to go ahead to get started
+    and do some machine learning with this titanic dataset.
+
